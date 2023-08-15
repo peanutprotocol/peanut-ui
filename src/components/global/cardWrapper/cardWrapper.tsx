d8d76723e70fc9a7398c8979b5dda5f63c5f79c2
borcherd <borcherd@me.com>
2023-08-15 18:00:10 +0200

    fix: more ui work

--- src/components/global/cardWrapper/cardWrapper.tsx
+++ src/components/global/cardWrapper/cardWrapper.tsx
@@ -1,6 +1,17 @@
-export function CardWrapper({ children }: { children: React.ReactNode }) {
+export function CardWrapper({
+  children,
+  mb = " mb-48",
+}: {
+  children: React.ReactNode;
+  mb?: string;
+}) {
   return (
-    <div className="flex flex-col items-center center-xy py-6 px-4 w-10/12 lg:w-2/3 xl:w-1/2 brutalborder bg-white mx-auto mt-5 mb-48 text-black relative">
+    <div
+      className={
+        "flex flex-col items-center center-xy py-6 px-4 w-10/12 lg:w-2/3 xl:w-1/2 brutalborder bg-white mx-auto mt-5 text-black relative " +
+        mb
+      }
+    >
       {" "}
       {children}
     </div>
