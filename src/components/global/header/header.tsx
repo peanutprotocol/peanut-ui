3a6668aa3018a7d33626f4ab31b760df3eee3382
borcherd <borcherd@me.com>
2023-08-15 16:42:19 +0200

    fix: temporary remove docs from header on mobile

--- src/components/global/header/header.tsx
+++ src/components/global/header/header.tsx
@@ -17,7 +17,7 @@ export function Header({ showMarquee = true }: { showMarquee?: boolean }) {
   return (
     <div>
       <nav className="relative flex flex-wrap justify-between bg-black max-h-20 mr-4 my-2">
-        <div className="flex items-center flex-grow gap-1 sm:gap-4">
+        <div className="flex items-center flex-grow gap-2 sm:gap-4">
           <Link
             className="flex items-center h-full font-bold text-2xl uppercase cursor-pointer hover:bg-white hover:text-black no-underline text-white pl-2 py-2"
             href="/"
@@ -32,7 +32,7 @@ export function Header({ showMarquee = true }: { showMarquee?: boolean }) {
             <span className="">about</span>
           </Link>
           <div
-            className="flex h-full font-bold items-center uppercase text-base cursor-pointer hover:bg-white hover:text-black no-underline text-white lg:px-8 py-2"
+            className="flex h-full font-bold items-center uppercase text-base cursor-pointer hover:bg-white hover:text-black no-underline text-white lg:px-8 py-2 invisible sm:visible"
             onClick={() => {
               window.open(
                 "https://peanutprotocol.notion.site/Peanut-Protocol-5776ec3a97de4e5d972ae3f6ba7f4f04"
