815aa7c42f615d51702146e6df5e435c8d868a05
Hugo Montenegro <h@hugomontenegro.com>
2023-08-18 19:54:42 +0200

    patch: smol fix for link and upgrade yarn lock

--- src/components/claim/claim.tsx
+++ src/components/claim/claim.tsx
@@ -64,7 +64,7 @@ export function Claim({ link }: { link: ReadonlyURLSearchParams }) {
         try {
             const linkDetails: interfaces.ILinkDetails = await peanut.getLinkDetails(
                 provider,
-                'https://peanut.to/claim?' + _link
+                'https://peanut.to/claim#?' + _link
             )
 
             if (Number(linkDetails.tokenAmount) <= 0) {
