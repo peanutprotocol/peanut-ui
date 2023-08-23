591f206b3b521278386700fcc5d7f4fde211071f
Hugo0 <Hugo0@users.noreply.github.com>
2023-08-23 21:40:08 +0000

    Prettified Code!

--- src/components/send/views/initial.view.tsx
+++ src/components/send/views/initial.view.tsx
@@ -262,7 +262,7 @@ export function SendInitialView({ onNextScreen, setClaimLink, setTxReceipt, setC
                     tokenDecimals: tokenDecimals,
                     verbose: true,
                     baseUrl: window.location.origin + '/claim',
-                    trackId: "ui",
+                    trackId: 'ui',
                 })
                 console.log('Created link:', link)
                 utils.saveToLocalStorage(address + ' - ' + txReceipt.hash, link)
