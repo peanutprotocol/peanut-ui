96278d8ff7156f806e6e96be2603451f551c1130
Ihor Brazhnichenko <ihor.bra@gmail.com>
2023-08-27 20:54:39 +0300

    Remove widget from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     return (
         <global_components.PageWrapper>
             <components.Send />
-            <components.SendWidget />
         </global_components.PageWrapper>
     )
 }
