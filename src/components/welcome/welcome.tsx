101fa375082b41747c923ffa8b71adc635655b51
k konrad <konrad.e.urban@gmail.com>
2023-10-05 20:03:14 +0200

    landing page content and correct links

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -2,6 +2,35 @@ import * as global_components from '@/components/global'
 import smiley from '@/assets/smiley.svg'
 import peanutman_happy from '@/assets/peanutman-happy.svg'
 
+// // Hero:
+// SEND TOKENS VIA LINKS
+
+// Forget chains and wallet addresses. Send tokens with a trustless payment link, no matter whether they have a wallet or not.
+
+// --
+// Use case 1: App
+// Use case  2: SDK integration
+// Use case  3: marketing campaigns
+
+// --
+// Features:
+// noncustodial
+// permissionless
+// brandable
+// noob-friendly
+
+// --
+// INTEGRATION
+// talk to an expert
+
+// --
+
+// testimonials
+
+// --
+
+// TRY APP NOW end of page CTA
+
 export function Welcome() {
     return (
         <div className="mt-0 flex h-full min-h-[100vh] flex-col  ">
@@ -10,21 +39,20 @@ export function Welcome() {
                 <div className="relative mx-auto">
                     <div className="border-2 border-black lg:grid lg:grid-flow-col-dense lg:grid-cols-2 ">
                         {/* left column */}
-                        <div className="brutalborder bg-yellow py-16 text-center sm:px-6 lg:mx-0 lg:max-w-none lg:px-0">
-                            <h1 className="mx-auto my-8 w-3/4 text-5xl font-black">Join the WELCOME!</h1>
+                        <div className="brutalborder bg-white py-16 text-center sm:px-6 lg:mx-0 lg:max-w-none lg:px-0">
+                            <h1 className="mx-auto my-8 w-3/4 text-5xl font-black">Send Tokens with a Link</h1>
                             <div className="m-4 mx-auto w-3/4 p-2 text-xl">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto dicta qui ullam dolores,
-                                culpa amet porro eos dignissimos deleniti minus quos possimus nemo dolorem quaerat
-                                asperiores ut sunt consequuntur temporibus?
+                                Forget chains and wallet addresses. Send tokens with a trustless payment link, no matter
+                                whether the recipient has a wallet.
                             </div>
 
                             <div className="mt-8 flex justify-center space-x-4 p-2">
                                 <a
-                                    href="/"
+                                    href="/send"
                                     id="cta-btn"
                                     className="mb-2 block bg-white p-5 text-2xl font-black md:w-3/5 lg:w-1/3"
                                 >
-                                    Join Raffle asdf
+                                    Try Now
                                 </a>
 
                                 <a
@@ -33,13 +61,13 @@ export function Welcome() {
                                     rel="noopener noreferrer"
                                     className="p-5 text-2xl font-black text-black hover:underline"
                                 >
-                                    About Peanut →
+                                    Integrate →
                                 </a>
                             </div>
                         </div>
 
                         {/* right column */}
-                        <div className="center-xy brutalborder relative overflow-hidden  border-2 border-black bg-fuchsia py-3 lg:border-l-0 lg:pb-16 lg:pt-32">
+                        <div className="center-xy brutalborder relative overflow-hidden  bg-fuchsia py-3 lg:border-l-0 lg:pb-16 lg:pt-32">
                             <img
                                 src={peanutman_happy.src}
                                 className="absolute top-10 duration-200 hover:rotate-12"
@@ -64,62 +92,48 @@ export function Welcome() {
                 </>
             </global_components.MarqueeWrapper>
 
-            {/* Features */}
+            {/* Use Cases: SDK, app, marketing */}
 
-            <section id="features">
-                <div role="list" className="grid grid-cols-1 gap-0 text-black sm:grid-cols-2 lg:grid-cols-3">
-                    {/* QR Codes / URLs */}
-                    <div
-                        className="brutalborder flex flex-col border-2 border-black bg-teal p-12 px-16 lg:border-r-0"
-                        id="QR"
-                    >
-                        <h3 className="text-5xl font-black">QR Codes / URLs</h3>
+            <section id="use-cases" className="bg-white">
+                <div role="list" className="grid grid-cols-1 gap-5 p-5 text-black sm:grid-cols-2 lg:grid-cols-3">
+                    {/* Integrate */}
+                    <div className="brutalborder flex flex-col border-2 border-black bg-teal p-12 px-16" id="SDK">
+                        <h3 className="text-5xl font-black">Integrate</h3>
                         <p className="mt-1 block text-2xl leading-loose">
-                            Reward prospective and existing community members with tokens. Just send them a URL or
-                            display a QR code on any medium you like. You'll be able to see who and how many have been
-                            claimed.
+                            Let your product go viral by your users sharing token links with their friends, who will get
+                            onboarded onto your product easily. Ideal for wallets, DEXes and dApps.
                         </p>
                         <div className="flex-grow"></div>
                         <div className="center-xy flex-end my-6 flex justify-around">
-                            <a href="/campaigns">
+                            <a href="/docs">
                                 <button className="brutalborder brutalshadow p-4 px-4 text-2xl font-black hover:invert">
                                     Start Now
                                 </button>
                             </a>
+
                             <a
-                                href="https://twitter.com/AmbireWallet/status/1641088818114641920"
+                                href="https://peanutprotocol.notion.site/Eco-Beam-Link-Case-Study-0df5cf9b5d544cb48e209cbcbcd63bdb?pvs=4"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                hidden
+                                className="ml-4 border-none bg-none p-5 text-2xl font-normal text-black no-underline hover:underline"
                             >
-                                <button className="ml-4 p-5 text-2xl font-normal hover:underline">Case Study →</button>
+                                Case Study →
                             </a>
                         </div>
                     </div>
-                    {/* Email */}
-                    <div
-                        className="brutalborder flex flex-col border-2 border-black bg-red p-12 px-16 lg:border-r-0"
-                        id="email"
-                    >
-                        <h3 className="text-5xl font-black">Email</h3>
+                    {/* App */}
+                    <div className="brutalborder flex flex-col border-2 border-black bg-red p-12 px-16 " id="app">
+                        <h3 className="text-5xl font-black">Use App</h3>
                         <p className="mt-1 block text-2xl leading-loose">
-                            Got an email list? Send tokens to your audience and convert them easily to users. Simply
-                            grab a CSV and plug the links to your next mergmail newsletter.
+                            See how easy it can be to send tokens. Simply generate a link using the Peanut app and
+                            pop into a chat with your friend.
                         </p>
                         <div className="flex-grow"></div>
                         <div className="center-xy flex-end my-6 flex justify-around">
-                            <a href="/campaigns">
+                            <a href="/">
                                 <button className="brutalborder brutalshadow p-4 px-4 text-2xl font-black hover:invert">
                                     Start Now
                                 </button>
-                            </a>
-                            <a
-                                href="https://twitter.com/AmbireWallet/status/1641088818114641920"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                hidden
-                            >
-                                <div className="ml-4 p-5 text-2xl font-normal hover:underline">Testimonial →</div>
                             </a>
                         </div>
                     </div>
@@ -140,12 +154,14 @@ export function Welcome() {
                                     Start Now
                                 </button>
                             </a>
+
                             <a
                                 href="https://twitter.com/AmbireWallet/status/1641088818114641920"
                                 target="_blank"
                                 rel="noopener noreferrer"
+                                className="ml-4 border-none bg-none p-5 text-2xl font-normal text-black no-underline hover:underline"
                             >
-                                <button className="ml-4 p-5 text-2xl font-normal hover:underline">Testimonial →</button>
+                                Testimonial →
                             </a>
                         </div>
                     </div>
@@ -166,12 +182,12 @@ export function Welcome() {
                 </>
             </global_components.MarqueeWrapper>
 
-            {/* Explainer Section */}
-            <section className="lg:divide-y" id="explainer">
-                <div className="brutalborder relative mx-auto text-black">
+            {/* Features  */}
+            <section className="lg:divide-y" id="features">
+                <div className="relative mx-auto text-black">
                     <div className="lg:grid lg:grid-flow-col-dense lg:grid-cols-2">
                         {/* Left column */}
-                        <div className="center-xy relative overflow-hidden border-2 border-black bg-fuchsia py-3 lg:border-l-0 lg:pb-16 lg:pt-32">
+                        <div className="center-xy relative overflow-hidden bg-fuchsia py-3 outline-black lg:border-l-0 lg:pb-16 lg:pt-32 ">
                             <img
                                 src={peanutman_happy.src}
                                 className="absolute top-10 duration-200 hover:rotate-12"
@@ -181,32 +197,50 @@ export function Welcome() {
                         </div>
 
                         {/* Right column */}
-                        <div className="brutalborder bg-yellow text-center sm:px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-16">
-                            <div className="text-center">
-                                <ul className="inline-block p-8 text-left text-xl lg:px-32 lg:leading-loose">
-                                    <p className="text-3xl font-black">Your Voice</p>
-                                    <li>
-                                        Customize token and claim page to match your branding with logo and colors. Or
-                                        even better, have the users claim the tokens on your own page!
-                                    </li>
-                                    <p className="text-3xl font-black">Noob-friendly</p>
-                                    <li>Guided wallet setup for first-time crypto users, no advance address needed!</li>
-                                    <p className="text-3xl font-black">Analytics</p>
-                                    <li>Understand how your campaign went and who your community is.</li>
-                                    <p className="text-3xl font-black">Non-custodial</p>
-                                    <li>
-                                        This is run on Peanut Protocol which is non-custodial and composable (see our{' '}
-                                        <a
-                                            href="https://peanutprotocol.notion.site/Send-Crypto-via-Link-SDK-9a89ea726b754a1c9f7e012125a01a85"
-                                            className="text-bold underline"
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                        >
-                                            SDK
-                                        </a>
-                                        )
-                                    </li>
-                                </ul>
+                        {/* // --
+// Features:
+// noncustodial
+// permissionless
+// brandable
+// noob-friendly
+ */}
+                        <div className=" bg-white text-center sm:px-6 lg:mx-0 lg:max-w-none lg:px-0 lg:py-8">
+                            <div className="inline-block w-4/5 text-left">
+                                <h2 className="text-4xl">Non-custodial</h2>
+                                <p className="text-normal">
+                                    Funds securely stored in a vault contract and can be only claimed with a secret that
+                                    is contained in the link.{' '}
+                                    <a href="/docs" target="_blank" className="text-black">
+                                        Read more.
+                                    </a>{' '}
+                                </p>
+
+                                <h2 className="text-4xl">Permissionless</h2>
+                                <p className="text-normal">
+                                    There are no API keys, simply use our SDK or build your own way to interact with the
+                                    smart contracts directly.{' '}
+                                    <a href="/docs" target="_blank" className="text-black">
+                                        Read more.
+                                    </a>
+                                </p>
+
+                                <h2 className="text-4xl">Brandable</h2>
+                                <p className="text-normal">
+                                    Customize token and claim page to match your branding with logo and colors. Or even
+                                    better, have the users claim the tokens on your own domain or app!{' '}
+                                    <a href="/docs" target="_blank" className="text-black">
+                                        Read more.
+                                    </a>
+                                </p>
+
+                                <h2 className="text-4xl">Noob-friendly onboarding</h2>
+                                <p className="text-normal">
+                                    Fully customizable guided wallet setup for first-time crypto users, no advance
+                                    address needed!
+                                    <a href="/docs" target="_blank" className="text-black">
+                                        Read more.
+                                    </a>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -233,13 +267,15 @@ export function Welcome() {
                     <div className="text-center">
                         <h2 className="title-font text-3xl font-black text-black lg:text-7xl">Make your own</h2>
 
-                        <div className="mx-auto w-11/12 p-5 px-6 text-3xl lg:w-2/3">
+                        <div className="mx-auto w-4/5 p-5 text-xl lg:w-2/3">
                             Do you want a custom onboarding flow? Collect email addresses? Make users download a
                             specific app or claim with a specific wallet? We've got your covered.
                         </div>
 
                         <a href="https://t.me/peanutprotocol" target="_blank" rel="noopener noreferrer">
-                            <button className="p-5 text-2xl font-black hover:underline">Talk to an expert →</button>
+                            <button id="cta-btn" className="cta m-10 bg-white px-3 text-2xl font-black hover:underline">
+                                Talk to an expert →
+                            </button>
                         </a>
                     </div>
                 </div>
@@ -307,12 +343,12 @@ export function Welcome() {
                                 alt={testimonial.altText}
                                 className="rainbow-border mx-auto w-1/2 rounded-full bg-white p-1"
                             />
-                            <h1 className="mx-auto mt-2 py-2 text-base font-black italic lg:text-lg">
+                            <h1 className="mx-auto mt-2 py-2 text-base font-normal italic lg:text-lg">
                                 {testimonial.comment}
                             </h1>
                             <p className="mb-4 text-base font-black uppercase">
                                 {testimonial.name}
-                                <span className="text-xs">
+                                <span className="text-xs font-normal">
                                     {' '}
                                     <br /> {testimonial.detail}{' '}
                                 </span>
