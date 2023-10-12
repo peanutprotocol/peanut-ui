95b9b70426f4df9ec5bc16add4fe7d675380e787
0xkkonrad <0xkkonrad@users.noreply.github.com>
2023-10-12 14:14:35 +0000

    Prettified Code!

--- public/transferswtf/script.js
+++ public/transferswtf/script.js
@@ -1,56 +1,55 @@
-
-let camera, scene, renderer, clock;
-let uniforms;
+let camera, scene, renderer, clock
+let uniforms
 
 function init() {
-	const container = document.getElementById("shader");
+    const container = document.getElementById('shader')
 
-	clock = new THREE.Clock();
-	camera = new THREE.Camera();
-	camera.position.z = 1;
+    clock = new THREE.Clock()
+    camera = new THREE.Camera()
+    camera.position.z = 1
 
-	scene = new THREE.Scene();
+    scene = new THREE.Scene()
 
-	const geometry = new THREE.PlaneBufferGeometry(2, 2);
+    const geometry = new THREE.PlaneBufferGeometry(2, 2)
 
-	uniforms = {
-		u_time: { type: "f", value: 1.0 },
-		u_resolution: { type: "v2", value: new THREE.Vector2() },
-	};
+    uniforms = {
+        u_time: { type: 'f', value: 1.0 },
+        u_resolution: { type: 'v2', value: new THREE.Vector2() },
+    }
 
-	const material = new THREE.ShaderMaterial({
-		uniforms,
-		vertexShader: document.getElementById("vertex").textContent,
-		fragmentShader: document.getElementById("fragment").textContent
-	});
+    const material = new THREE.ShaderMaterial({
+        uniforms,
+        vertexShader: document.getElementById('vertex').textContent,
+        fragmentShader: document.getElementById('fragment').textContent,
+    })
 
-	const mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio(window.devicePixelRatio);
+    renderer = new THREE.WebGLRenderer()
+    renderer.setPixelRatio(window.devicePixelRatio)
 
-	container.appendChild(renderer.domElement);
+    container.appendChild(renderer.domElement)
 
-	onWindowResize();
-	window.addEventListener("resize", onWindowResize);
+    onWindowResize()
+    window.addEventListener('resize', onWindowResize)
 }
 
 function onWindowResize() {
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	uniforms.u_resolution.value.x = renderer.domElement.width;
-	uniforms.u_resolution.value.y = renderer.domElement.height;
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    uniforms.u_resolution.value.x = renderer.domElement.width
+    uniforms.u_resolution.value.y = renderer.domElement.height
 }
 
 function render() {
-	uniforms.u_time.value = clock.getElapsedTime();
-	renderer.render(scene, camera);
+    uniforms.u_time.value = clock.getElapsedTime()
+    renderer.render(scene, camera)
 }
 
 function animate() {
-	render();
-	requestAnimationFrame(animate);
+    render()
+    requestAnimationFrame(animate)
 }
 
-init();
-animate();
+init()
+animate()
