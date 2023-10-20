76e0811200de0b9d0603f6e3f908e94bdba5b64a
borcherd <borcherd@me.com>
2023-10-20 18:00:43 +0200

    fix: api endpoint

--- src/app/api/apple-app-site-association/route.ts
+++ src/app/api/apple-app-site-association/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 export async function GET(request: NextRequest) {
-    const response = await fetch('https://api.peanut.to/AASA')
+    const response = await fetch('https://api.peanut.to/apple-app-site-association')
     const data = await response.json()
     return NextResponse.json(data, {
         status: 200,
