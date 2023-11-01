e670f4d92ecf80c189075d75b01cd6e554b2b86c
Hugo Montenegro <h@hugomontenegro.com>
2023-11-01 14:40:20 +0000

    chore: packages area annoying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         ReactGA.initialize(process.env.GA_KEY ?? '')
         console.log(art, 'font-size: 20px; background-color: blue; color: yellow;')
         peanut.toggleVerbose(true)
-        console.og
+        console.log('Peanut-SDL version: v' + peanut.version)
     }, [])
 
     return (
