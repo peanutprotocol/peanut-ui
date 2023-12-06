dd71b18566448671bee3c74ffd6ff0d5e161a69c
borcherd <borcherd@me.com>
2023-12-06 14:38:16 +0000

    feat: more lp work

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -15,52 +15,91 @@ import wallet_connect_logo from '@/assets/logos/wallet-connect-logo.png'
 import teal_wallet_one from '@/assets/mockups/teal-wallet-1.png'
 import teal_wallet_two from '@/assets/mockups/teal-wallet-2.png'
 import dropdown_svg from '@/assets/dropdown.svg'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { getCalApi } from '@calcom/embed-react'
 
 const logoCloudLogos = [hypersphere_logo, zeeprime_logo, wallet_connect_logo, beam_logo, eco_logo, kofime_logo]
 const features = [
     {
-        name: 'white-labled',
+        name: 'Whitelabed',
         description:
-            'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
+            'Your brand deserves to be front and center for new users. It’s nuts but you can completely whitelabel these links and use your own domain and branding.',
         bg: 'bg-yellow',
+        redirectUrl: 'https://docs.peanut.to/integrations/domain-agnostic-links',
+        buttonText: 'Docs',
     },
     {
-        name: 'gasless',
+        name: 'Gasless',
         description:
-            'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
+            'Users should not have to worry about gas, being on the right chain or wallet addresses. Claim links solve the cold start problem.',
         bg: 'bg-fuchsia',
+        redirectUrl: 'https://docs.peanut.to/sdk-documentation/building-with-the-sdk/claiming-peanut-links-gaslessly',
+        buttonText: 'Docs',
     },
     {
-        name: 'campaigns',
-        description:
-            'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
+        name: 'Welcome packs',
+        description: 'Send a welcome pack of NFT + gas + token to new or existing customers',
         bg: 'bg-red',
+        calModal: true,
+        redirectUrl: 'https://docs.peanut.to/overview/use-cases',
+        buttonText: 'Case study',
     },
     {
-        name: 'xchain',
+        name: 'Cross-chain',
         description:
             'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
         bg: 'bg-lightblue',
+        redirectUrl: 'https://experimental.peanut.to/send',
+        buttonText: 'Try Now',
+        learnMoreUrl: 'https://docs.peanut.to/sdk-documentation/building-with-the-sdk/x-chain-links',
     },
     {
-        name: 'physical',
+        name: 'Get Physical',
         description:
-            'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
+            'Are you planning an IRL event? Do a physical airdrop by distributing QR codes with tokens. Just put stickers on your swag or flyers and boost your conversion rate.',
         bg: 'bg-teal',
+        calModal: true,
+        redirectUrl: undefined,
     },
     {
-        name: 'NFTs',
-        description:
-            'Customize token and claim page to match your branding with logo and colors. Or even better, have the users claim the tokens on your own domain or app!',
+        name: 'Web2 Airdrops',
+        description: 'Airdrop your web2 audience (think Discord, Mailchimp, Twitter)',
         bg: 'bg-yellow',
+        calModal: true,
+        redirectUrl: undefined,
     },
 ]
 const faqs = [
-    { question: 'How does it work?', answer: 'It just does.' },
-    { question: 'How does it work?', answer: 'It just does.' },
-    { question: 'How does it work?', answer: 'It just does.' },
-    { question: 'How does it work?', answer: 'It just does.' },
+    { question: 'How can I try?', answer: 'Check out our dapp or any of the projects that already integrated Peanut.' },
+    {
+        question: 'What are the trust assumptions?',
+        answer: 'Peanut Protocol is non-custodial, permissionless and decentralised. Read more ',
+        redirectUrl: '',
+        redirectText: 'here',
+    },
+    {
+        question: 'What happens if I want to cancel or if I lose the link?',
+        answer: 'You can always withdraw or cancel your own links. See how ',
+        redirectUrl: '',
+        redirectText: 'here',
+    },
+    {
+        question: 'What are the fees?',
+        answer: 'On our dapp, we sponsor gasless claiming. Integrators can choose to sponsor the transactions. We do not have a fee on the protocol, see ',
+        redirectUrl: '',
+        redirectText: 'here',
+    },
+    {
+        question: 'I need help!',
+        answer: 'Sure! Let us know at hello@peanut.to or on ',
+        redirectUrl: 'https://discord.gg/uWFQdJHZ6j',
+        redirectText: 'discord',
+    },
+    {
+        question: 'I want this for our app! How long does it take to integrate?',
+        answer: 'Our record integration took 2 hours, but it depends on your stack. Let’s chat!',
+        calModal: true,
+    },
 ]
 const testimonials = [
     {
@@ -104,6 +143,18 @@ export function Welcome() {
         return classes.filter(Boolean).join(' ')
     }
 
+    useEffect(() => {
+        ;(async function () {
+            const cal = await getCalApi()
+            cal('ui', {
+                theme: 'dark',
+                styles: { branding: { brandColor: '#ffffff' } },
+                hideEventTypeDetails: false,
+                layout: 'month_view',
+            })
+        })()
+    }, [])
+
     return (
         <div className="mt-0 flex h-full min-h-[100vh] flex-col  ">
             {/* hero */}
@@ -111,16 +162,15 @@ export function Welcome() {
                 <div className="w-full bg-white py-8 text-center sm:px-6 lg:mx-0 lg:w-2/3 lg:max-w-none lg:px-0">
                     <h1 className="mx-auto mb-8 mt-0 w-3/4 text-5xl font-black">Send Tokens with a Link</h1>
                     <div className="m-4 mx-auto w-3/4 p-2 text-xl">
-                        Forget chains and wallet addresses. Send tokens with a trustless payment link, no matter whether
-                        the recipient has a wallet.
+                        Go viral with claim links. Let your users send tokens through links
                     </div>
 
                     <div className="mt-8 flex justify-center space-x-4 p-2">
                         <a
-                            href="https://cal.com/kkonrad"
-                            target="_blank"
+                            data-cal-link="kkonrad/15min"
+                            data-cal-config='{"layout":"month_view"}'
                             id="cta-btn"
-                            className="mb-2 block bg-white p-5 text-2xl font-black md:w-3/5 lg:w-1/3"
+                            className="mb-2 block cursor-pointer bg-white p-5 text-2xl font-black underline md:w-3/5 lg:w-1/3"
                         >
                             cal.com
                         </a>
@@ -189,18 +239,28 @@ export function Welcome() {
                     <div className=" flex w-full flex-col gap-2 text-center sm:gap-8 sm:px-6 lg:mx-0 lg:w-2/3 lg:max-w-none lg:px-0">
                         <h1 className="mx-auto my-0 w-3/4  pt-4 text-5xl font-black">Go viral</h1>
                         <div className="mx-auto w-3/4 pb-4 text-xl ">
-                            Forget chains and wallet addresses. Send tokens with a trustless payment link, no matter
-                            whether the recipient has a wallet.Forget chains and wallet addresses. Send tokens with a
-                            trustless payment link, no matter whether the recipient has a wallet.
+                            Links are the easiest way to send crypto. Leverage your userbase to get more users. Let your
+                            users send tokens to their friends and get them onboarded, no matter whether they’re users
+                            already.
                         </div>
 
-                        <div className="flex justify-center space-x-4 p-2">
+                        <div className="mt-8 flex justify-center space-x-4 p-2">
                             <a
-                                href="/send"
+                                data-cal-link="kkonrad/15min"
+                                data-cal-config='{"layout":"month_view"}'
                                 id="cta-btn"
-                                className="mb-2 block bg-white p-5 text-2xl font-black md:w-3/5 lg:w-1/3"
+                                className="mb-2 block cursor-pointer bg-white p-5 text-2xl font-black underline md:w-3/5 lg:w-1/3"
                             >
-                                Try Now
+                                cal.com
+                            </a>
+
+                            <a
+                                href="https://docs.peanut.to"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="p-5 text-2xl font-black text-black hover:underline"
+                            >
+                                Integrate →
                             </a>
                         </div>
                     </div>
@@ -220,18 +280,26 @@ export function Welcome() {
                     <div className=" flex w-full flex-col gap-2 text-center sm:gap-8 sm:px-6 lg:mx-0 lg:w-2/3 lg:max-w-none lg:px-0">
                         <h1 className="mx-auto my-0 w-3/4  pt-4 text-5xl font-black">Claim gasless</h1>
                         <div className="mx-auto w-3/4 pb-4 text-xl ">
-                            Forget chains and wallet addresses. Send tokens with a trustless payment link, no matter
-                            whether the recipient has a wallet.Forget chains and wallet addresses. Send tokens with a
-                            trustless payment link, no matter whether the recipient has a wallet.
+                            Onboard new users seamlessly with a gassless claiming and onboarding experience.
                         </div>
 
-                        <div className="flex justify-center space-x-4 p-2">
+                        <div className="mt-8 flex justify-center space-x-4 p-2">
                             <a
-                                href="/send"
+                                data-cal-link="kkonrad/15min"
+                                data-cal-config='{"layout":"month_view"}'
                                 id="cta-btn"
-                                className="mb-2 block bg-white p-5 text-2xl font-black md:w-3/5 lg:w-1/3"
+                                className="mb-2 block cursor-pointer bg-white p-5 text-2xl font-black underline md:w-3/5 lg:w-1/3"
                             >
-                                Try Now
+                                cal.com
+                            </a>
+
+                            <a
+                                href="https://docs.peanut.to"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="p-5 text-2xl font-black text-black hover:underline"
+                            >
+                                Integrate →
                             </a>
                         </div>
                     </div>
@@ -245,15 +313,42 @@ export function Welcome() {
                         return (
                             <div
                                 className={classNames(
-                                    'brutalborder flex flex-col items-center justify-center px-8 py-4 ',
+                                    'brutalborder flex flex-col border-2 border-black p-12 px-16 ',
                                     feature.bg
                                 )}
-                                key={index}
+                                id="app"
                             >
-                                <h2 className="mb-0 mt-0 flex h-12 items-center justify-center text-3xl">
-                                    {feature.name}
-                                </h2>
-                                <p className="text-normal">{feature.description}</p>
+                                <h3 className="text-5xl font-black"> {feature.name}</h3>
+                                <p className="mt-1 block text-2xl leading-loose">{feature.description}</p>
+                                <div className="flex-grow"></div>
+                                <div className="center-xy flex-end my-6 flex justify-around">
+                                    {feature.learnMoreUrl && (
+                                        <button className="brutalborder brutalshadow cursor-pointer bg-white p-4 px-4 text-2xl font-black ">
+                                            <a href={feature.learnMoreUrl} target="_blank" className="text-black">
+                                                Learn more
+                                            </a>
+                                        </button>
+                                    )}
+                                    {feature.redirectUrl && (
+                                        <button className="brutalborder brutalshadow cursor-pointer bg-white p-4 px-4 text-2xl font-black ">
+                                            <a href={feature.redirectUrl} target="_blank" className="text-black">
+                                                {feature.buttonText}
+                                            </a>
+                                        </button>
+                                    )}
+
+                                    {feature.calModal && (
+                                        <button className="brutalborder brutalshadow cursor-pointer bg-white p-4 px-4 text-2xl font-black ">
+                                            <a
+                                                data-cal-link="kkonrad/15min"
+                                                data-cal-config='{"layout":"month_view"}'
+                                                className={'underline '}
+                                            >
+                                                Cal.com
+                                            </a>
+                                        </button>
+                                    )}
+                                </div>
                             </div>
                         )
                     })}
