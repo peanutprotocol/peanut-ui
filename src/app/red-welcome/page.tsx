e2bc785159cf538424611c3e870e828c7a396685
borcherd <borcherd@me.com>
2024-02-11 22:01:29 +0100

    fix: modal width

---
+++ src/app/red-welcome/page.tsx
@@ -0,0 +1,27 @@
+import * as global_components from '@/components/global'
+import * as components from '@/components'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Peanut Protocol',
+    description: 'Send crypto with a link',
+
+    icons: {
+        icon: '/logo-favicon.png',
+    },
+    openGraph: {
+        images: [
+            {
+                url: '/metadata-img.png',
+            },
+        ],
+    },
+}
+
+export default function Home() {
+    return (
+        <global_components.PageWrapper>
+            <components.WelcomeRaffle />
+        </global_components.PageWrapper>
+    )
+}
