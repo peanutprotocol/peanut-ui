3df4a7cdbdd0d78ceb3dac7e5c77e4c5c815e77f
borcherd <borcherd@me.com>
2024-03-07 14:59:00 +0100

    feat: test fixes

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -32,53 +32,59 @@ type Props = {
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
-function createURL(searchParams: { [key: string]: string | string[] | undefined }): string {
-    const baseURL = 'https://peanut.to/claim'
+// function createURL(searchParams: { [key: string]: string | string[] | undefined }): string {
+//     const baseURL = 'https://peanut.to/claim'
 
-    const queryParams = new URLSearchParams()
+//     const queryParams = new URLSearchParams()
 
-    Object.keys(searchParams).forEach((key) => {
-        const value = searchParams[key]
-        if (Array.isArray(value)) {
-            value.forEach((item) => queryParams.append(key, item))
-        } else if (value) {
-            queryParams.append(key, value)
-        }
-    })
+//     Object.keys(searchParams).forEach((key) => {
+//         const value = searchParams[key]
+//         if (Array.isArray(value)) {
+//             value.forEach((item) => queryParams.append(key, item))
+//         } else if (value) {
+//             queryParams.append(key, value)
+//         }
+//     })
 
-    return `${baseURL}?${queryParams.toString()}`
-}
+//     return `${baseURL}?${queryParams.toString()}`
+// }
 
-export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-    let title = 'Peanut Protocol'
+// export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+//     let title = 'Peanut Protocol'
 
-    try {
-        const url = createURL(searchParams)
-        const linkDetails = await getLinkDetails({ link: url })
-        title =
-            'You received ' +
-            (Number(linkDetails.tokenAmount) < 0.01
-                ? 'some '
-                : utils.formatAmount(Number(linkDetails.tokenAmount)) + ' in ') +
-            linkDetails.tokenSymbol +
-            '!'
-    } catch (e) {
-        console.log('error: ', e)
-    }
+//     try {
+//         const url = createURL(searchParams)
+//         const linkDetails = await getLinkDetails({ link: url })
+//         title =
+//             'You received ' +
+//             (Number(linkDetails.tokenAmount) < 0.01
+//                 ? 'some '
+//                 : utils.formatAmount(Number(linkDetails.tokenAmount)) + ' in ') +
+//             linkDetails.tokenSymbol +
+//             '!'
+//     } catch (e) {
+//         console.log('error: ', e)
+//     }
 
-    return {
-        title: title,
-        icons: {
-            icon: '/logo-favicon.png',
-        },
-        openGraph: {
-            images: [
-                {
-                    url: '/claim-metadata-img.jpg',
-                },
-            ],
-        },
-    }
+//     return {
+//         title: title,
+//         icons: {
+//             icon: '/logo-favicon.png',
+//         },
+//         openGraph: {
+//             images: [
+//                 {
+//                     url: '/claim-metadata-img.jpg',
+//                 },
+//             ],
+//         },
+//     }
+// }
+
+export const metadata: Metadata = {
+    // without a title, warpcast won't validate your frame
+    title: 'Peanut Protocol',
+    description: 'Send crypto with a link',
 }
 
 type State = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import * as global_components from '@/components/global'
 import * as components from '@/components'
 import { Metadata } from 'next'
-
+import {
+    FrameButton,
+    FrameContainer,
+    FrameImage,
+    FrameInput,
+    FrameReducer,
+    NextServerPageProps,
+    getFrameMessage,
+    getPreviousFrame,
+    useFramesReducer,
+} from 'frames.js/next/server'
 export const metadata: Metadata = {
     title: 'Peanut Protocol',
     description: 'Send crypto with a link',
@@ -17,11 +27,38 @@ export const metadata: Metadata = {
         ],
     },
 }
+type State = {
+    active: string
+    total_button_presses: number
+}
 
-export default function Home() {
+const initialState = { active: '1', total_button_presses: 0 }
+
+const reducer: FrameReducer<State> = (state, action) => {
+    return {
+        total_button_presses: state.total_button_presses + 1,
+        active: action.postBody?.untrustedData.buttonIndex ? String(action.postBody?.untrustedData.buttonIndex) : '1',
+    }
+}
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function Home({ params, searchParams }: Props) {
+    const previousFrame = getPreviousFrame<State>(searchParams)
+
+    const [state] = useFramesReducer<State>(reducer, initialState, previousFrame)
     return (
         <global_components.PageWrapper>
             <components.welcomePage />
+            <FrameContainer postUrl="/frames" pathname="/claim" state={state} previousFrame={previousFrame}>
+                <FrameImage aspectRatio="1.91:1" src="https://peanut.to/claim-metadata-img.jpg" />
+                <FrameButton action="link" target={'/'}>
+                    Claim
+                </FrameButton>
+            </FrameContainer>
         </global_components.PageWrapper>
     )
 }
