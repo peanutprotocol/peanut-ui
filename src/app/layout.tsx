3bd1b85bd4b33b193efd510009fc4ad22f79226e
borcherd <borcherd@me.com>
2024-03-07 15:39:14 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,73 @@
-import type { Metadata } from 'next'
-import './globals.css'
+'use client'
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
-export const metadata: Metadata = {
-    // without a title, warpcast won't validate your frame
-    title: 'frames.js starter',
-    description: '...',
-}
+import * as config from '@/config'
+import { Store } from '@/store/store'
+import { useState, useEffect } from 'react'
+import peanut from '@squirrel-labs/peanut-sdk'
+import ReactGA from 'react-ga4'
+import '../../sentry.client.config'
+import '../../sentry.server.config'
+import '../../sentry.edge.config'
+import 'react-tooltip/dist/react-tooltip.css'
+import { useInitWeb3InboxClient } from '@web3inbox/widget-react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+var art = `%c
+Слава Україні
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀PEANUT⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣦⣌⠙⠋⢡⣴⣶⡄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⣿⣿⣿⡿⢋⣠⣶⣶⡌⠻⣿⠟⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡆⠸⠟⢁⣴⣿⣿⣿⣿⣿⡦⠉⣴⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⠟⠀⠰⣿⣿⣿⣿⣿⣿⠟⣠⡄⠹⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢸⡿⢋⣤⣿⣄⠙⣿⣿⡿⠟⣡⣾⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⠿⠀⢠⣾⣿⣿⣿⣦⠈⠉⢠⣾⣿⣿⣿⠏⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣦⣄⠙⠋⣠⣴⣿⣿⣿⣿⠿⠛⢁⣴⣦⡄⠙⠛⠋⠁⠀⠀⠀⠀
+⠀⠀⢀⣾⣿⣿⠟⢁⣴⣦⡈⠻⣿⣿⡿⠁⡀⠚⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠘⣿⠟⢁⣴⣿⣿⣿⣿⣦⡈⠛⢁⣼⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢰⡦⠀⢴⣿⣿⣿⣿⣿⣿⣿⠟⢀⠘⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠘⢀⣶⡀⠻⣿⣿⣿⣿⡿⠋⣠⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢿⣿⣿⣦⡈⠻⣿⠟⢁⣼⣿⣿⠟⠀⠀⠀⠀⠀⠀PEANUT⠀⠀⠀⠀
+⠀⠀⠈⠻⣿⣿⣿⠖⢀⠐⠿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+`
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const [ready, setReady] = useState(false)
+
+    //this useEffect is needed to prevent hydration error when autoConnect in wagmiConfig is true
+    useEffect(() => {
+        setReady(true)
+        ReactGA.initialize(process.env.GA_KEY ?? '')
+        console.log(art, 'font-size: 20px; background-color: blue; color: yellow;')
+        peanut.toggleVerbose(true)
+        peanut.greeting()
+    }, [])
+
+    const web3inboxIsReady = useInitWeb3InboxClient({
+        // The project ID and domain you setup in the Domain Setup section
+        projectId: process.env.WC_PROJECT_ID ?? '',
+        domain: 'peanut.to',
+
+        // Allow localhost development with "unlimited" mode.
+        // This authorizes this dapp to control notification subscriptions for all domains (including `app.example.com`), not just `window.location.host`
+        isLimited: false,
+    })
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={inter.className}>
+                {ready && (
+                    <config.ContextProvider>
+                        <Store>
+                            {children}
+                            <Analytics />
+                        </Store>
+                    </config.ContextProvider>
+                )}
+            </body>
         </html>
     )
 }
