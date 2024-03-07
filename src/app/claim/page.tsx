74e21ac33b023a5b9433eba258c33a84a727c989
borcherd <borcherd@me.com>
2024-03-07 15:43:07 +0100

    revert changes

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -32,59 +32,53 @@ type Props = {
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
-// function createURL(searchParams: { [key: string]: string | string[] | undefined }): string {
-//     const baseURL = 'https://peanut.to/claim'
+function createURL(searchParams: { [key: string]: string | string[] | undefined }): string {
+    const baseURL = 'https://peanut.to/claim'
 
-//     const queryParams = new URLSearchParams()
+    const queryParams = new URLSearchParams()
 
-//     Object.keys(searchParams).forEach((key) => {
-//         const value = searchParams[key]
-//         if (Array.isArray(value)) {
-//             value.forEach((item) => queryParams.append(key, item))
-//         } else if (value) {
-//             queryParams.append(key, value)
-//         }
-//     })
+    Object.keys(searchParams).forEach((key) => {
+        const value = searchParams[key]
+        if (Array.isArray(value)) {
+            value.forEach((item) => queryParams.append(key, item))
+        } else if (value) {
+            queryParams.append(key, value)
+        }
+    })
 
-//     return `${baseURL}?${queryParams.toString()}`
-// }
+    return `${baseURL}?${queryParams.toString()}`
+}
 
-// export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-//     let title = 'Peanut Protocol'
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+    let title = 'Peanut Protocol'
 
-//     try {
-//         const url = createURL(searchParams)
-//         const linkDetails = await getLinkDetails({ link: url })
-//         title =
-//             'You received ' +
-//             (Number(linkDetails.tokenAmount) < 0.01
-//                 ? 'some '
-//                 : utils.formatAmount(Number(linkDetails.tokenAmount)) + ' in ') +
-//             linkDetails.tokenSymbol +
-//             '!'
-//     } catch (e) {
-//         console.log('error: ', e)
-//     }
+    try {
+        const url = createURL(searchParams)
+        const linkDetails = await getLinkDetails({ link: url })
+        title =
+            'You received ' +
+            (Number(linkDetails.tokenAmount) < 0.01
+                ? 'some '
+                : utils.formatAmount(Number(linkDetails.tokenAmount)) + ' in ') +
+            linkDetails.tokenSymbol +
+            '!'
+    } catch (e) {
+        console.log('error: ', e)
+    }
 
-//     return {
-//         title: title,
-//         icons: {
-//             icon: '/logo-favicon.png',
-//         },
-//         openGraph: {
-//             images: [
-//                 {
-//                     url: '/claim-metadata-img.jpg',
-//                 },
-//             ],
-//         },
-//     }
-// }
-
-export const metadata: Metadata = {
-    // without a title, warpcast won't validate your frame
-    title: 'Peanut Protocol',
-    description: 'Send crypto with a link',
+    return {
+        title: title,
+        icons: {
+            icon: '/logo-favicon.png',
+        },
+        openGraph: {
+            images: [
+                {
+                    url: '/claim-metadata-img.jpg',
+                },
+            ],
+        },
+    }
 }
 
 type State = {
@@ -101,7 +95,7 @@ const reducer: FrameReducer<State> = (state, action) => {
     }
 }
 
-export default function ClaimPage({ params, searchParams }: Props) {
+export default function ClaimPage({ searchParams }: Props) {
     const url = currentURL('/')
     const previousFrame = getPreviousFrame<State>(searchParams)
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import * as global_components from '@/components/global'
+import * as components from '@/components'
+import { Metadata } from 'next'
 import {
     FrameButton,
     FrameContainer,
@@ -9,10 +12,22 @@ import {
     getPreviousFrame,
     useFramesReducer,
 } from 'frames.js/next/server'
-import Link from 'next/link'
-import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from './debug'
-import { currentURL } from './utils'
 
+export const metadata: Metadata = {
+    title: 'Peanut Protocol',
+    description: 'Send crypto with a link',
+    // metadataBase: new URL('https://peanut.to'),
+    // icons: {
+    //     icon: '/logo-favicon.png',
+    // },
+    // openGraph: {
+    //     images: [
+    //         {
+    //             url: '/metadata-img.png',
+    //         },
+    //     ],
+    // },
+}
 type State = {
     active: string
     total_button_presses: number
@@ -27,62 +42,22 @@ const reducer: FrameReducer<State> = (state, action) => {
     }
 }
 
-// This is a react server component only
-export default async function Home({ searchParams }: NextServerPageProps) {
-    const url = currentURL('/')
+export default function Home({ searchParams }: NextServerPageProps) {
     const previousFrame = getPreviousFrame<State>(searchParams)
-
-    const frameMessage = await getFrameMessage(previousFrame.postBody, {
-        hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
-    })
-
-    if (frameMessage && !frameMessage?.isValid) {
-        throw new Error('Invalid frame payload')
-    }
 
     const [state, dispatch] = useFramesReducer<State>(reducer, initialState, previousFrame)
 
-    // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
-    // example: load the users credentials & check they have an NFT
-
-    console.log('info: state is:', state)
-
-    // then, when done, return next frame
     return (
-        <div className="p-4">
-            frames.js starter kit. The Template Frame is on this page, it&apos;s in the html meta tags (inspect source).{' '}
-            <Link href={createDebugUrl(url)} className="underline">
-                Debug
-            </Link>
+        <global_components.PageWrapper>
+            <components.welcomePage />
             <FrameContainer postUrl="/frames" pathname="/" state={state} previousFrame={previousFrame}>
                 {/* <FrameImage src="https://framesjs.org/og.png" /> */}
-                <FrameImage aspectRatio="1.91:1">
-                    <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
-                        <div tw="flex flex-row">{frameMessage?.inputText ? frameMessage.inputText : 'Hello world'}</div>
-                        {frameMessage && (
-                            <div tw="flex flex-col">
-                                <div tw="flex">Requester is @{frameMessage.requesterUserData?.username} </div>
-                                <div tw="flex">
-                                    Requester follows caster: {frameMessage.requesterFollowsCaster ? 'true' : 'false'}
-                                </div>
-                                <div tw="flex">
-                                    Caster follows requester: {frameMessage.casterFollowsRequester ? 'true' : 'false'}
-                                </div>
-                                <div tw="flex">Requester liked cast: {frameMessage.likedCast ? 'true' : 'false'}</div>
-                                <div tw="flex">
-                                    Requester recasted cast: {frameMessage.recastedCast ? 'true' : 'false'}
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </FrameImage>
-                <FrameInput text="put some text here" />
-                <FrameButton>{state?.active === '1' ? 'Active' : 'Inactive'}</FrameButton>
-                <FrameButton>{state?.active === '2' ? 'Active' : 'Inactive'}</FrameButton>
-                <FrameButton action="link" target={`https://www.google.com`}>
-                    External
+                <FrameImage aspectRatio="1.91:1" src="https://peanut.to/claim-metadata-img.jpg" />
+
+                <FrameButton action="link" target={'/'}>
+                    Claim
                 </FrameButton>
             </FrameContainer>
-        </div>
+        </global_components.PageWrapper>
     )
 }
