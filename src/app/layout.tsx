56f88a170307086379c1a2f182056927fc5681e0
borcherd <borcherd@me.com>
2024-03-07 15:47:44 +0100

    fixes?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
-'use client'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 
 import * as config from '@/config'
 import { Store } from '@/store/store'
-import { useState, useEffect } from 'react'
-import peanut from '@squirrel-labs/peanut-sdk'
-import ReactGA from 'react-ga4'
 import '../../sentry.client.config'
 import '../../sentry.server.config'
 import '../../sentry.edge.config'
@@ -36,37 +32,24 @@ var art = `%c
 `
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    const [ready, setReady] = useState(false)
+    // const web3inboxIsReady = useInitWeb3InboxClient({
+    //     // The project ID and domain you setup in the Domain Setup section
+    //     projectId: process.env.WC_PROJECT_ID ?? '',
+    //     domain: 'peanut.to',
 
-    //this useEffect is needed to prevent hydration error when autoConnect in wagmiConfig is true
-    useEffect(() => {
-        setReady(true)
-        ReactGA.initialize(process.env.GA_KEY ?? '')
-        console.log(art, 'font-size: 20px; background-color: blue; color: yellow;')
-        peanut.toggleVerbose(true)
-        peanut.greeting()
-    }, [])
-
-    const web3inboxIsReady = useInitWeb3InboxClient({
-        // The project ID and domain you setup in the Domain Setup section
-        projectId: process.env.WC_PROJECT_ID ?? '',
-        domain: 'peanut.to',
-
-        // Allow localhost development with "unlimited" mode.
-        // This authorizes this dapp to control notification subscriptions for all domains (including `app.example.com`), not just `window.location.host`
-        isLimited: false,
-    })
+    //     // Allow localhost development with "unlimited" mode.
+    //     // This authorizes this dapp to control notification subscriptions for all domains (including `app.example.com`), not just `window.location.host`
+    //     isLimited: false,
+    // })
     return (
         <html lang="en">
             <body className={inter.className}>
-                {ready && (
-                    <config.ContextProvider>
-                        <Store>
-                            {children}
-                            <Analytics />
-                        </Store>
-                    </config.ContextProvider>
-                )}
+                <config.ContextProvider>
+                    <Store>
+                        {children}
+                        <Analytics />
+                    </Store>
+                </config.ContextProvider>
             </body>
         </html>
     )
