41c48c0870efb9698ca613e7f5d245f9152500b7
borcherd <borcherd@me.com>
2024-03-07 15:28:48 +0100

    frames testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import * as global_components from '@/components/global'
-import * as components from '@/components'
-import { Metadata } from 'next'
 import {
     FrameButton,
     FrameContainer,
@@ -12,6 +9,9 @@ import {
     getPreviousFrame,
     useFramesReducer,
 } from 'frames.js/next/server'
+import Link from 'next/link'
+import { DEFAULT_DEBUGGER_HUB_URL, createDebugUrl } from './debug'
+import { currentURL } from './utils'
 
 type State = {
     active: string
@@ -27,19 +27,62 @@ const reducer: FrameReducer<State> = (state, action) => {
     }
 }
 
-export default function Home({ searchParams }: NextServerPageProps) {
+// This is a react server component only
+export default async function Home({ searchParams }: NextServerPageProps) {
+    const url = currentURL('/')
     const previousFrame = getPreviousFrame<State>(searchParams)
+
+    const frameMessage = await getFrameMessage(previousFrame.postBody, {
+        hubHttpUrl: DEFAULT_DEBUGGER_HUB_URL,
+    })
+
+    if (frameMessage && !frameMessage?.isValid) {
+        throw new Error('Invalid frame payload')
+    }
 
     const [state, dispatch] = useFramesReducer<State>(reducer, initialState, previousFrame)
 
-    return (
-        <FrameContainer postUrl="/frames" pathname="/" state={state} previousFrame={previousFrame}>
-            {/* <FrameImage src="https://framesjs.org/og.png" /> */}
-            <FrameImage aspectRatio="1.91:1" src="https://peanut.to/claim-metadata-img.jpg" />
+    // Here: do a server side side effect either sync or async (using await), such as minting an NFT if you want.
+    // example: load the users credentials & check they have an NFT
 
-            <FrameButton action="link" target={'/'}>
-                Claim
-            </FrameButton>
-        </FrameContainer>
+    console.log('info: state is:', state)
+
+    // then, when done, return next frame
+    return (
+        <div className="p-4">
+            frames.js starter kit. The Template Frame is on this page, it&apos;s in the html meta tags (inspect source).{' '}
+            <Link href={createDebugUrl(url)} className="underline">
+                Debug
+            </Link>
+            <FrameContainer postUrl="/frames" pathname="/" state={state} previousFrame={previousFrame}>
+                {/* <FrameImage src="https://framesjs.org/og.png" /> */}
+                <FrameImage aspectRatio="1.91:1">
+                    <div tw="w-full h-full bg-slate-700 text-white justify-center items-center flex flex-col">
+                        <div tw="flex flex-row">{frameMessage?.inputText ? frameMessage.inputText : 'Hello world'}</div>
+                        {frameMessage && (
+                            <div tw="flex flex-col">
+                                <div tw="flex">Requester is @{frameMessage.requesterUserData?.username} </div>
+                                <div tw="flex">
+                                    Requester follows caster: {frameMessage.requesterFollowsCaster ? 'true' : 'false'}
+                                </div>
+                                <div tw="flex">
+                                    Caster follows requester: {frameMessage.casterFollowsRequester ? 'true' : 'false'}
+                                </div>
+                                <div tw="flex">Requester liked cast: {frameMessage.likedCast ? 'true' : 'false'}</div>
+                                <div tw="flex">
+                                    Requester recasted cast: {frameMessage.recastedCast ? 'true' : 'false'}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </FrameImage>
+                <FrameInput text="put some text here" />
+                <FrameButton>{state?.active === '1' ? 'Active' : 'Inactive'}</FrameButton>
+                <FrameButton>{state?.active === '2' ? 'Active' : 'Inactive'}</FrameButton>
+                <FrameButton action="link" target={`https://www.google.com`}>
+                    External
+                </FrameButton>
+            </FrameContainer>
+        </div>
     )
 }
