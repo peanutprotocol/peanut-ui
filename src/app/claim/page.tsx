161284b80b078f6548955e7d8fa24fefa5c654a1
borcherd <borcherd@me.com>
2024-03-08 11:33:36 +0100

    chore: revert claimpage

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -4,28 +4,6 @@ import { Metadata, ResolvingMetadata } from 'next'
 import { getLinkDetails } from '@squirrel-labs/peanut-sdk'
 import * as utils from '@/utils'
 export const dynamic = 'force-dynamic'
-import {
-    FrameButton,
-    FrameContainer,
-    FrameImage,
-    FrameInput,
-    FrameReducer,
-    NextServerPageProps,
-    getFrameMessage,
-    getPreviousFrame,
-    useFramesReducer,
-} from 'frames.js/next/server'
-import Link from 'next/link'
-
-import { headers } from 'next/headers'
-
-function currentURL(pathname: string): URL {
-    const headersList = headers()
-    const host = headersList.get('x-forwarded-host') || headersList.get('host')
-    const protocol = headersList.get('x-forwarded-proto') || 'http'
-
-    return new URL(pathname, `${protocol}://${host}`)
-}
 
 type Props = {
     params: { id: string }
@@ -81,35 +59,10 @@ export async function generateMetadata({ params, searchParams }: Props, parent: 
     }
 }
 
-type State = {
-    active: string
-    total_button_presses: number
-}
-
-const initialState = { active: '1', total_button_presses: 0 }
-
-const reducer: FrameReducer<State> = (state, action) => {
-    return {
-        total_button_presses: state.total_button_presses + 1,
-        active: action.postBody?.untrustedData.buttonIndex ? String(action.postBody?.untrustedData.buttonIndex) : '1',
-    }
-}
-
-export default function ClaimPage({ searchParams }: Props) {
-    const url = currentURL('/')
-    const previousFrame = getPreviousFrame<State>(searchParams)
-
-    const [state] = useFramesReducer<State>(reducer, initialState, previousFrame)
-
+export default function ClaimPage({ params, searchParams }: Props) {
     return (
         <global_components.PageWrapper>
             <components.Claim />
-            <FrameContainer postUrl="/frames" pathname="/claim" state={state} previousFrame={previousFrame}>
-                <FrameImage aspectRatio="1.91:1" src="https://peanut.to/claim-metadata-img.jpg" />
-                <FrameButton action="link" target={url.toString()}>
-                    Claim
-                </FrameButton>
-            </FrameContainer>
         </global_components.PageWrapper>
     )
 }
