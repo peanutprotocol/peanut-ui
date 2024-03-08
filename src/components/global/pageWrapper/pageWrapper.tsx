1f79b57346e2b0fbd885771af06c62233aed4cb5
borcherd <borcherd@me.com>
2024-03-08 13:45:40 +0100

    chore: higher level pagewrapper and build error

--- src/components/global/pageWrapper/pageWrapper.tsx
+++ src/components/global/pageWrapper/pageWrapper.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import * as global_components from '@/components/global'
 import { useState, useEffect } from 'react'
 
@@ -15,10 +17,12 @@ export function PageWrapper({
         setIsReady(true)
     }, [])
     return (
-        <div className="scrollbar-hide flex min-h-screen flex-col bg-black">
-            <global_components.Header showMarquee={showMarquee} />
-            <div className={' flex min-h-screen flex-col justify-center  ' + bgColor}>{isReady && children}</div>
-            <global_components.Footer showMarquee={showMarquee} />
-        </div>
+        isReady && (
+            <div className="scrollbar-hide flex min-h-screen flex-col bg-black">
+                <global_components.Header showMarquee={showMarquee} />
+                <div className={' flex min-h-screen flex-col justify-center  ' + bgColor}>{children}</div>
+                <global_components.Footer showMarquee={showMarquee} />
+            </div>
+        )
     )
 }
