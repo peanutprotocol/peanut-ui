3d5f0f015b2fc9d8d34b65552ccf3366091df06a
borcherd <borcherd@me.com>
2024-03-10 11:41:53 +0100

    feat: static mobulachains json

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -13,6 +13,129 @@ export const defaultTokenDetailsAtom = atom<interfaces.IPeanutTokenDetail[]>([])
 
 export const supportedMobulaChainsAtom = atom<{ chainId: string; name: string }[]>([])
 
+const mobulaChains = [
+    {
+        name: 'Fantom',
+        chainId: '250',
+    },
+    {
+        name: 'Avalanche C-Chain',
+        chainId: '43114',
+    },
+    {
+        name: 'Cronos',
+        chainId: '25',
+    },
+    {
+        name: 'DFK Subnet',
+        chainId: '53935',
+    },
+    {
+        name: 'Ethereum',
+        chainId: '1',
+    },
+    {
+        name: 'SmartBCH',
+        chainId: '10000',
+    },
+    {
+        name: 'Polygon',
+        chainId: '137',
+    },
+    {
+        name: 'BNB Smart Chain (BEP20)',
+        chainId: '56',
+    },
+    {
+        name: 'Celo',
+        chainId: '42220',
+    },
+    {
+        name: 'XDAI',
+        chainId: '100',
+    },
+    {
+        name: 'Klaytn',
+        chainId: '8217',
+    },
+    {
+        name: 'Aurora',
+        chainId: '1313161554',
+    },
+    {
+        name: 'HECO',
+        chainId: '128',
+    },
+    {
+        name: 'Harmony',
+        chainId: '1666600000',
+    },
+    {
+        name: 'Boba',
+        chainId: '288',
+    },
+    {
+        name: 'OKEX',
+        chainId: '66',
+    },
+    {
+        name: 'Moonriver',
+        chainId: '1285',
+    },
+    {
+        name: 'Moonbeam',
+        chainId: '1284',
+    },
+    {
+        name: 'BitTorrent Chain',
+        chainId: '199',
+    },
+    {
+        name: 'Oasis',
+        chainId: '42262',
+    },
+    {
+        name: 'Velas',
+        chainId: '106',
+    },
+    {
+        name: 'Arbitrum',
+        chainId: '42161',
+    },
+    {
+        name: 'Optimistic',
+        chainId: '10',
+    },
+    {
+        name: 'Kucoin',
+        chainId: '321',
+    },
+    {
+        name: 'Base',
+        chainId: '8453',
+    },
+    {
+        name: 'Shibarium',
+        chainId: '109',
+    },
+    {
+        name: 'Mantle',
+        chainId: '5000',
+    },
+    {
+        name: 'Sui',
+        chainId: 'sui',
+    },
+    {
+        name: 'ZetaChain',
+        chainId: '7000',
+    },
+    {
+        name: 'Alephium',
+        chainId: 'alephium-0',
+    },
+]
+
 export function Store({ children }: { children: React.ReactNode }) {
     const [userBalances, setUserBalances] = useAtom(userBalancesAtom)
     const setDefaultChainDetails = useSetAtom(defaultChainDetailsAtom)
@@ -45,22 +168,23 @@ export function Store({ children }: { children: React.ReactNode }) {
     // Fetch supported chains from Mobula, use this to display supported peanut chains in the UI that arent supported by mobula
     // Not needed to do in BFF since this api call doesnt need an API key
     const getSupportedChainsMobula = async () => {
-        if (supportedMobulaChains.length == 0) {
-            const supportedChains = await fetch('https://api.mobula.io/api/1/blockchains', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            const supportedChainsJson = await supportedChains.json()
+        // if (supportedMobulaChains.length == 0) {
+        // const supportedChains = await fetch('https://api.mobula.io/api/1/blockchains', {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        // })
+        // const supportedChainsJson = await supportedChains.json()
 
-            //setting supported chains and mapping to simple object
-            setSupportedMobulaChains(
-                supportedChainsJson.data.map((chain: any) => {
-                    return { chainId: chain.chainId, name: chain.name }
-                })
-            )
-        }
+        // //setting supported chains and mapping to simple object
+        // setSupportedMobulaChains(
+        //     supportedChainsJson.data.map((chain: any) => {
+        //         return { chainId: chain.chainId, name: chain.name }
+        //     })
+        // )
+        setSupportedMobulaChains(mobulaChains)
+        // }
     }
 
     const getPeanutChainAndTokenDetails = async () => {
@@ -114,6 +238,8 @@ export function Store({ children }: { children: React.ReactNode }) {
 
                 const mobulaResponseJson = await mobulaResponse.json()
 
+                console.log(mobulaResponseJson)
+
                 const mappedUserBalances = mapToUserBalances(mobulaResponseJson.data).sort(
                     (a: interfaces.IUserBalance, b: interfaces.IUserBalance) => {
                         if (a.chainId === b.chainId) {
@@ -126,6 +252,8 @@ export function Store({ children }: { children: React.ReactNode }) {
                         }
                     }
                 )
+
+                console.log(mappedUserBalances)
 
                 if (mobulaResponse.ok) {
                     setUserBalances(mappedUserBalances)
