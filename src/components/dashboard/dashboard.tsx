1339c32572999432e2b408fdb89bc4db230a7325
Hugo Montenegro <h@hugomontenegro.com>
2024-03-16 20:20:27 +0000

    fix: mobile copy button

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -304,27 +304,31 @@ export function Dashboard() {
                                                                         : 'Claimed'}{' '}
                                                                     {Number(item.amount) > 0 && item.token}
                                                                 </td>
-                                                                <td
-                                                                    className="brutalborder-bottom h-8 cursor-pointer px-1 py-2"
-                                                                    onClick={() => {
-                                                                        gaEventTracker('link-copied', '')
-                                                                        navigator.clipboard.writeText(item.link)
-                                                                        setCopiedLink([item.link])
-                                                                    }}
-                                                                >
-                                                                    {Number(item.amount) > 0 ? (
+                                                                <td className="brutalborder-bottom h-8 cursor-pointer px-1 py-2">
+                                                                    {item.claimed ? (
+                                                                        <></>
+                                                                    ) : (
                                                                         <svg
                                                                             xmlns="http://www.w3.org/2000/svg"
                                                                             fill="none"
                                                                             viewBox="0 0 24 24"
-                                                                            x-show="!linkCopied"
                                                                             stroke="currentColor"
                                                                             className="inline h-5 w-5"
+                                                                            onClick={() => {
+                                                                                gaEventTracker('link-copied', '')
+                                                                                navigator.clipboard.writeText(item.link)
+                                                                                setCopiedLink([item.link])
+                                                                            }}
                                                                         >
                                                                             <path d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+
+                                                                            {/* <path
+                                                                                strokeLinecap="round"
+                                                                                strokeLinejoin="round"
+                                                                                strokeWidth={2}
+                                                                                d="M8 7H5a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-3M7 7l5-5m0 0l5 5m-5-5v12"
+                                                                            /> */}
                                                                         </svg>
-                                                                    ) : (
-                                                                        ''
                                                                     )}
                                                                 </td>
                                                             </tr>
