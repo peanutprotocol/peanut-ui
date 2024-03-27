962c99fa443fae9bf3042faeb7aaeba161c3a9b3
borcherd <borcherd@me.com>
2024-03-27 09:28:21 +0100

    feat: update sentry configs

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,21 +1,13 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from '@sentry/nextjs'
-import { CaptureConsole } from '@sentry/integrations'
 
 Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    integrations: [
-        new CaptureConsole({
-            levels: ['error'],
-        }),
-    ],
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-
-    // ...
-
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
+    enabled: process.env.NODE_ENV != 'development' ? true : false,
+    tracesSampleRate: 1,
+    debug: false,
 })
