aee247fe2f4730a4ce1832596951677667550b86
borcherd <borcherd@me.com>
2024-03-29 08:05:40 +0100

    chore: remove unused imports

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,13 +1,11 @@
 'use client'
 import * as global_components from '@/components/global'
 import * as welcomePages from './components'
-import { useConnectorClient } from 'wagmi'
-import { useConnections, useAccount } from 'wagmi'
 
 export function welcomePage() {
     return (
         <global_components.PageWrapper>
-            <welcomePages.Welcome />{' '}
+            <welcomePages.Welcome />
         </global_components.PageWrapper>
     )
 }
