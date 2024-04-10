81fc9152e51c6133bc3f8beb092f66f7b2316097
borcherd <borcherd@me.com>
2024-04-10 15:04:31 +0200

    test: document.referrer logging

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,8 +1,13 @@
 'use client'
 import * as global_components from '@/components/global'
 import * as welcomePages from './components'
+import { useEffect } from 'react'
 
 export function welcomePage() {
+    useEffect(() => {
+        console.log(document.referrer)
+    }, [])
+
     return (
         <global_components.PageWrapper>
             <welcomePages.Welcome />
