8ec15114e4d7f525ae65875f63caf81cb417fbd7
borcherd <borcherd@me.com>
2024-05-07 10:24:25 +0200

    chore: colormodescript update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body className={`${roboto.variable} font-sans`}>
                 <ColorModeProvider>
-                    <ColorModeScript
-                        initialColorMode="light"
-                        key="chakra-ui-no-flash"
-                        storageKey="chakra-ui-color-mode"
-                    />
+                    <ColorModeScript initialColorMode="light" />
                     <config.PeanutProvider>
                         <context.ContextProvider>{children}</context.ContextProvider>
                     </config.PeanutProvider>
