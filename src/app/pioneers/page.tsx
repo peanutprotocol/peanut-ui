0555168c64acf89f3e6e7adbd45cc3074a747ea9
borcherd <borcherd@me.com>
2024-05-16 17:18:44 +0200

    feat: pioneerspage

---
+++ src/app/pioneers/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Widget } from '@typeform/embed-react'
+import * as global_components from '@/components/global'
+
+export default function PioneersPage() {
+    return (
+        <global_components.PageWrapper>
+            <Widget
+                id="lTEp058W"
+                style={{ width: '75%', height: '600px' }}
+                className="center-xy items-center self-center"
+            />
+        </global_components.PageWrapper>
+    )
+}
