184b87b9414917429571bd378498583ac3aceb50
borcherd <borcherd@me.com>
2024-05-23 18:29:46 +0200

    feat: hotjar

--- src/config/peanut.config.tsx
+++ src/config/peanut.config.tsx
@@ -5,6 +5,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { useEffect } from 'react'
 import peanut from '@squirrel-labs/peanut-sdk'
 import ReactGA from 'react-ga4'
+import Hotjar from '@hotjar/browser';
 
 import '../../sentry.client.config'
 import '../../sentry.server.config'
@@ -25,6 +26,10 @@ export function PeanutProvider({ children }: { children: React.ReactNode }) {
         allApps: false,
         logLevel: 'info',
     })
+    
+    const siteId = 4995627;
+    const hotjarVersion = 6;
+    Hotjar.init(siteId, hotjarVersion);
 
     return (
         <config.ContextProvider>
