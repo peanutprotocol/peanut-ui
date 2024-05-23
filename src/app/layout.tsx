57299b87b54ba90135b464a79f877a8bcb5a590b
borcherd <borcherd@me.com>
2024-05-23 20:08:09 +0200

    feat: logrocket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import * as config from '@/config'
 import * as context from '@/context'
 import Head from 'next/head'
 import Hotjar from '@hotjar/browser'
+import LogRocket from 'logrocket';
 
 const roboto = Roboto_Flex({
     weight: ['400', '500', '700', '800'],
@@ -16,6 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     const siteId = 4995627
     const hotjarVersion = 6
     Hotjar.init(siteId, hotjarVersion)
+
+    LogRocket.init('x2zwq1/peanut-protocol');
+
     return (
         <html lang="en">
             <Head>
