4e12292e4c6bbe50e89d22fe96b1d2a68d0162f2
borcherd <borcherd@me.com>
2024-06-25 23:57:44 +0200

    feat: fix undefined tokenLogoUri

--- src/components/Global/ImageGeneration/LinkPreview.tsx
+++ src/components/Global/ImageGeneration/LinkPreview.tsx
@@ -11,11 +11,14 @@ export function LinkPreviewImg({
     tokenAddress: string
     tokenSymbol: string
 }) {
+    console.log(consts.peanutTokenDetails.find((detail) => detail.chainId === chainId)?.tokens)
     const tokenImage = consts.peanutTokenDetails
         .find((detail) => detail.chainId === chainId)
         ?.tokens.find((token) => utils.compareTokenAddresses(token.address, tokenAddress))?.logoURI
     const chainImage = consts.supportedPeanutChains.find((chain) => chain.chainId === chainId)?.icon.url
 
+    console.log('tokenImage: ', tokenImage)
+    console.log('chainImage: ', chainImage)
     return (
         <div
             style={{
@@ -46,30 +49,35 @@ export function LinkPreviewImg({
                     width: '160px',
                 }}
             >
-                <img
-                    src={chainImage ?? ''}
-                    alt="Chain Image"
-                    style={{
-                        height: '150px',
-                        width: '150px',
-                        borderRadius: '50%',
-                        objectFit: 'cover',
-                        position: 'absolute',
-                    }}
-                />
-                <img
-                    src={tokenImage ?? ''}
-                    alt="Token Image"
-                    style={{
-                        position: 'absolute',
-                        right: '-40px',
-                        top: '-40px',
-                        height: '100px',
-                        width: '100px',
-                        borderRadius: '50%',
-                        objectFit: 'cover',
-                    }}
-                />
+                {chainImage && (
+                    <img
+                        src={chainImage ?? ''}
+                        alt="Chain Image"
+                        style={{
+                            height: '150px',
+                            width: '150px',
+                            borderRadius: '50%',
+                            objectFit: 'cover',
+                            position: 'absolute',
+                        }}
+                    />
+                )}
+
+                {tokenImage && (
+                    <img
+                        src={tokenImage ?? ''}
+                        alt="Token Image"
+                        style={{
+                            position: 'absolute',
+                            right: '-40px',
+                            top: '-40px',
+                            height: '100px',
+                            width: '100px',
+                            borderRadius: '50%',
+                            objectFit: 'cover',
+                        }}
+                    />
+                )}
             </div>
         </div>
     )
