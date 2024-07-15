b7c2e26af4ee87dfaa3228e1b15a44e4d9354f85
borcherd <borcherd@users.noreply.github.com>
2024-07-15 13:54:10 +0000

    Prettified Code!

--- src/components/Claim/Link/Initial.view.tsx
+++ src/components/Claim/Link/Initial.view.tsx
@@ -235,8 +235,7 @@ export const InitialClaimLinkView = ({
 
                         toAddress: recipient.address
                             ? recipient.address
-                            : address ?? '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
-
+                            : (address ?? '0xd8da6bf26964af9d7eed9e03e53415d37aa96045'),
                     })
                     setRoutes([...routes, route])
                     setSelectedRoute(route)
