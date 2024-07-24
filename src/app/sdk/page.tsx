cf2da8859c8733ad620c9994ea8b7e33d979fb55
borcherd <borcherd@me.com>
2024-07-24 12:50:04 +0200

    feat: update landing page

---
+++ src/app/sdk/page.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from 'next'
+import Layout from '@/components/Global/Layout'
+import * as components from '@/components'
+
+export const metadata: Metadata = {
+    title: 'Peanut Protocol',
+    description: 'Send crypto via link',
+    metadataBase: new URL('https://peanut.to'),
+    icons: {
+        icon: '/logo-favicon.png',
+    },
+    openGraph: {
+        images: [
+            {
+                url: '/metadata-img.png',
+            },
+        ],
+    },
+}
+export default function Home() {
+    return (
+        <Layout className="!mx-0 w-full !px-0 !pt-0 ">
+            <components.WelcomeSDK />
+        </Layout>
+    )
+}
