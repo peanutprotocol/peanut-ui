f745721472ade8d841a2b213af842e575f82d789
borcherd <borcherd@me.com>
2024-08-12 15:11:24 +0200

    fix: update formatDate utils date format

---
+++ src/components/Dashboard/Dashboard.utils.ts
@@ -0,0 +1,10 @@
+export function formatDate(date: Date): string {
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = (date.getMonth() + 1).toString().padStart(2, '0') // JavaScript months are zero-indexed
+    const year = date.getFullYear()
+    const hours = date.getHours().toString().padStart(2, '0')
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    const seconds = date.getSeconds().toString().padStart(2, '0')
+
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`
+}
