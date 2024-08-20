1669b37031ec51e9d995ef8d42839b2b3e077f39
borcherd <borcherd@me.com>
2024-08-20 13:50:37 +0200

    feat: sentry fixes

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,6 +7,13 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     enabled: process.env.NODE_ENV != 'development' ? true : false,
+
+    // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
+
+    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: process.env.NODE_ENV === 'development',
 })
