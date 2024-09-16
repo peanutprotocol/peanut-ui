1c62240adba94ba88328a3806a9642c4e6c2d07e
Patrick <pa@pallen.co>
2024-09-16 21:27:40 +0100

    Updated the homepage styling.

--- src/components/Welcome/welcome.tsx
+++ src/components/Welcome/welcome.tsx
@@ -26,6 +26,7 @@ import {
     EyesEmoiji,
     GoodIdeaSign,
     SmileSide,
+    PeanutGuy,
 } from '@/assets'
 import { motion } from 'framer-motion'
 import * as chain_logos from '@/assets/chains'
@@ -149,77 +150,102 @@ const testimonials = [
 export function Welcome() {
     return (
         <div className=" flex w-full flex-col items-center justify-center">
-            <div className="flex w-full text-black dark:text-white">
-                <div className="relative flex w-full flex-col items-center justify-center gap-8 py-8 text-center sm:px-6 sm:py-16 md:gap-12 md:py-20 lg:mx-0 lg:px-0 lg:py-24">
-                    <motion.img
-                        initial={{ opacity: 0, translateY: 20, translateX: 5 }}
-                        whileInView={{ opacity: 1, translateY: 0, translateX: 0 }}
-                        transition={{ type: 'spring', damping: 5 }}
-                        src={Star.src}
-                        className="absolute left-[6%] top-[12%] w-12 md:left-[10%] md:top-[23%] lg:left-[10%] lg:top-[23%]"
-                    />
-                    <motion.img
-                        initial={{ opacity: 0, translateY: 28, translateX: -5 }}
-                        whileInView={{ opacity: 1, translateY: 0, translateX: 0 }}
-                        transition={{ type: 'spring', damping: 5 }}
-                        src={Star.src}
-                        className="absolute right-[4%] top-[8%] w-12 md:right-[5%] md:top-[16%] lg:right-[14%] lg:top-[16%]"
-                    />
-                    <motion.img
-                        initial={{ rotate: 5, opacity: 0, translateY: 28, translateX: -5, transformOrigin: 'top left' }}
-                        whileInView={{
-                            rotate: -6,
-                            opacity: 1,
-                            translateY: 0,
-                            translateX: 0,
-                            transformOrigin: 'top left',
-                        }}
-                        // whileHover={{ rotate: 5, transformOrigin: 'top left' }}
-                        transition={{ type: 'spring', damping: 10 }}
-                        src={EyesEmoiji.src}
-                        className="absolute left-[7%] top-[58%] hidden w-36 md:left-[1%] md:top-[65%] lg:left-[7%] lg:top-[58%] lg:block xl:left-[11%]"
-                    />
+            <div className="relative">
+                <motion.img
+                    initial={{ opacity: 0, translateY: 28, translateX: -5 }}
+                    whileInView={{ opacity: 1, translateY: 0, translateX: 0 }}
+                    transition={{ type: 'spring', damping: 5 }}
+                    src={PeanutGuy.src}
+                    className="absolute bottom-[3%] right-[-2%] z-0 hidden h-1/3 md:bottom-[6%] md:right-[-12%] md:h-1/2 lg:right-[-14%] lg:block"
+                />
 
-                    <div className="space-y-8">
-                        <div className="flex w-full flex-col items-center justify-center gap-2">
-                            <div className="mx-auto flex flex-row items-center justify-center gap-2 font-display text-h1 uppercase md:text-[4rem] lg:text-[5rem]">
-                                Send{' '}
-                                <div className="scroller w-[6rem] text-h1 md:w-[8.5rem] md:text-[4rem] lg:w-[10rem] lg:text-[5rem]">
-                                    <span className="">
-                                        NFTs
-                                        <br />
-                                        USDC
-                                        <br />
-                                        DAI
-                                        <br />
-                                        PEPE
-                                    </span>
-                                </div>{' '}
-                                Via Link
+                <div className="space-y-8 py-8 sm:space-y-12 sm:py-16 md:space-y-24 md:py-20 lg:space-y-28 lg:py-24">
+                    <div className="relative flex w-full text-black dark:text-white">
+                        <motion.img
+                            initial={{ opacity: 0, translateY: 20, translateX: 5 }}
+                            whileInView={{ opacity: 1, translateY: 0, translateX: 0 }}
+                            transition={{ type: 'spring', damping: 5 }}
+                            src={Star.src}
+                            className="absolute left-[2%] top-[7%] w-12 md:left-[5%] md:top-[10%] lg:left-[5%] lg:top-[10%]"
+                        />
+                        <motion.img
+                            initial={{ opacity: 0, translateY: 28, translateX: -5 }}
+                            whileInView={{ opacity: 1, translateY: 0, translateX: 0 }}
+                            transition={{ type: 'spring', damping: 5 }}
+                            src={Star.src}
+                            className="absolute right-[3%] top-[4%] w-12 md:right-[4%] md:top-[8%] lg:right-[4%] lg:top-[8%]"
+                        />
+
+                        <motion.img
+                            initial={{
+                                rotate: 5,
+                                opacity: 0,
+                                translateY: 28,
+                                translateX: -5,
+                                transformOrigin: 'top left',
+                            }}
+                            whileInView={{
+                                rotate: -6,
+                                opacity: 1,
+                                translateY: 0,
+                                translateX: 0,
+                                transformOrigin: 'top left',
+                            }}
+                            // whileHover={{ rotate: 5, transformOrigin: 'top left' }}
+                            transition={{ type: 'spring', damping: 10 }}
+                            src={EyesEmoiji.src}
+                            className="absolute left-[7%] top-[58%] hidden w-36 md:left-[1%] md:top-[65%] lg:left-[7%] lg:top-[58%] lg:block xl:left-[11%]"
+                        />
+
+                        <div className="flex w-full flex-col items-center justify-center gap-8 text-center sm:px-6 md:gap-12 lg:mx-0 lg:px-0">
+                            <div className="space-y-8">
+                                <div className="flex w-full flex-col items-center justify-center gap-2">
+                                    <div className="mx-auto flex flex-row items-center justify-center gap-2 font-display text-h1 uppercase md:text-[4rem] lg:text-[5rem]">
+                                        Send{' '}
+                                        <div className="scroller w-[6rem] text-h1 md:w-[8.5rem] md:text-[4rem] lg:w-[10rem] lg:text-[5rem]">
+                                            <span className="">
+                                                NFTs
+                                                <br />
+                                                USDC
+                                                <br />
+                                                DAI
+                                                <br />
+                                                PEPE
+                                            </span>
+                                        </div>{' '}
+                                        Via Link
+                                    </div>
+                                </div>
+
+                                <div className="max-w-4-xl mx-auto w-3/4 text-h4 uppercase">
+                                    Send money to your friends without having to worry about anything else!
+                                </div>
+                            </div>
+
+                            <div className="flex w-full items-center justify-center space-x-4 p-2 sm:gap-4">
+                                <Link href={'/send'} className="btn-2xl btn-purple max-w-64">
+                                    App
+                                </Link>
+
+                                <Link
+                                    href="https://docs.peanut.to"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-xl font-bold md:text-2xl"
+                                >
+                                    Integrate →
+                                </Link>
                             </div>
                         </div>
-
-                        <div className="max-w-4-xl mx-auto w-3/4 text-h4 uppercase">
-                            Send money to your friends without having to worry about anything else!
-                        </div>
                     </div>
 
-                    <div className="flex w-full items-center justify-center space-x-4 p-2 sm:gap-4">
-                        <Link href={'/send'} className="btn-2xl btn-purple max-w-64">
-                            App
-                        </Link>
-
-                        <Link
-                            href="https://docs.peanut.to"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-xl font-bold md:text-2xl"
-                        >
-                            Integrate →
-                        </Link>
+                    <div className="py-6- md:py-10- md:pb-20- lg:pb-30- flex w-full flex-wrap items-center gap-4 bg-transparent px-4">
+                        <label className="feature feature-primary grow -rotate-2">300k+ Transactions</label>
+                        <label className="feature grow rotate-1">105k+ Unique wallet addresses</label>
+                        <label className="feature feature-primary grow -rotate-1">20+ Chains</label>
                     </div>
 
-                    <div className="mx-5 flex flex-row flex-wrap items-center justify-center gap-4 gap-y-8 sm:gap-8">
+                    <div className="relative mx-5 flex flex-row flex-wrap items-center justify-center gap-4 gap-y-8 sm:gap-8">
                         {logoCloudLogos.map((logo) => {
                             return (
                                 <a
@@ -242,12 +268,6 @@ export function Welcome() {
                 </div>
             </div>
 
-            <div className="flex w-full flex-wrap items-center gap-4 bg-transparent px-4 py-6 md:py-10 md:pb-20 lg:pb-30">
-                <label className="feature feature-primary grow -rotate-2">300k+ Transactions</label>
-                <label className="feature grow rotate-1">105k+ Unique wallet addresses</label>
-                <label className="feature feature-primary grow -rotate-1">20+ Chains</label>
-            </div>
-
             {/* <div className="py-6"> */}
             {/* <MarqueeWrapper backgroundColor="bg-transparant" direction="right" className="">
                     {Object.entries(chain_logos).map(([chain, logo]) => {
@@ -261,11 +281,11 @@ export function Welcome() {
             {/* </div> */}
 
             <div className="flex w-full flex-col items-center justify-center">
-                <MarqueeComp message={`Frens send links`} imageSrc={HandThumbs.src} />
+                {/* <MarqueeComp message={`Frens send links`} imageSrc={HandThumbs.src} /> */}
 
                 <FAQsPanel heading="FAQs" questions={faqs} />
 
-                <MarqueeComp message={`Frens`} imageSrc={SmileStars.src} imageAnimationClass="animation-faceSpin" />
+                {/* <MarqueeComp message={`Frens`} imageSrc={SmileStars.src} imageAnimationClass="animation-faceSpin" /> */}
             </div>
 
             <div role="list" className="relative z-1 mx-auto pb-20 pt-8 md:pb-36 md:pt-20 lg:px-4 xl:w-[92%] 2xl:w-4/5">
