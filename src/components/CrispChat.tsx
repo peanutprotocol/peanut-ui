cf72391c62ce7d421500f665e4bcb89522b51bb6
Hugo Montenegro <h@hugomontenegro.com>
2024-09-18 11:42:34 +0100

    feat: Add CrispChat component and integrate it into layout

---
+++ src/components/CrispChat.tsx
@@ -0,0 +1,19 @@
+import Script from 'next/script'
+
+export default function CrispChat() {
+    return (
+        <Script strategy="afterInteractive">
+            {`
+        window.$crisp=[];
+        window.CRISP_WEBSITE_ID="916078be-a6af-4696-82cb-bc08d43d9125";
+        (function(){
+          d=document;
+          s=d.createElement("script");
+          s.src="https://client.crisp.chat/l.js";
+          s.async=1;
+          d.getElementsByTagName("head")[0].appendChild(s);
+        })();
+      `}
+        </Script>
+    )
+}
