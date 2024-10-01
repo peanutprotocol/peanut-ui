c6979ee55e5a20c1f9cb2523daf77eb5dcaeb083
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-01 17:05:16 +0200

    feat: add cashout to navbar + core refactor

---
+++ src/components/Global/Header/components/index.tsx
@@ -0,0 +1,36 @@
+import { Box } from '@chakra-ui/react'
+import Link from 'next/link'
+import { HTMLAttributes } from 'react'
+
+export const NavItemBox = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <Box
+            className="h-full w-full hover:bg-white hover:text-black"
+            px={{
+                base: 8,
+                sm: 4,
+                lg: 2,
+            }}
+        >
+            {children}
+        </Box>
+    )
+}
+
+export const NavLink = ({
+    href,
+    children,
+    ...rest
+}: { href: string; children: React.ReactNode } & HTMLAttributes<HTMLAnchorElement>) => {
+    return (
+        <NavItemBox>
+            <Link
+                {...rest}
+                href={href}
+                className="flex h-full w-full items-center justify-start py-2 uppercase  sm:w-max sm:justify-center"
+            >
+                {children}
+            </Link>
+        </NavItemBox>
+    )
+}
