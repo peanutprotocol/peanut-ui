8b77671c5e2b9af56d5f91aec01dc6ec73123321
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-05 15:33:39 +0200

    fix: make larger on mobile with new button taking space

--- src/components/Global/IframeWrapper/index.tsx
+++ src/components/Global/IframeWrapper/index.tsx
@@ -32,22 +32,32 @@ const IframeWrapper = ({
         <Modal
             visible={visible}
             onClose={onClose}
-            classWrap="w-full max-w-2xl"
+            classWrap="w-full max-w-2xl border-none sm:border"
             classOverlay="bg-black bg-opacity-50"
             video={false}
+            /**
+             * Making sure the Iframe showing on top of the Crisp Chat widget
+             * which has z-index of 1000000
+             */
+            className="z-[1000001]"
             classButtonClose="hidden"
         >
-            <div className="flex flex-col gap-2 p-2 sm:p-5">
-                <div className="overflow-hidden rounded-sm border border-black">
+            <div className="flex flex-col gap-2 p-0 sm:p-5">
+                <div className="w-full sm:hidden">
+                    <button className="btn-purple w-full rounded-none" onClick={onClose}>
+                        CLOSE
+                    </button>
+                </div>
+                <div className="h-[550px] overflow-hidden rounded-sm sm:h-[500px] sm:border sm:border-black">
                     <iframe
                         src={src}
                         allow="camera;"
-                        style={style}
-                        className="rounded-md border border-black"
+                        style={{ ...style, width: '100%', height: '100%', border: 'none' }}
+                        className="rounded-md"
                         sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation"
                     />
                 </div>
-                <div className="w-full">
+                <div className="hidden w-full sm:flex">
                     <button className="btn-purple w-full" onClick={onClose}>
                         Close
                     </button>
