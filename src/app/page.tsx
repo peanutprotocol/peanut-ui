251e34b94cf920e3c2d6683623ea61d9f1dbe98b
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-09 18:29:34 +0200

    make app pwa compatible with serwist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { Metadata } from 'next'
+import { Metadata, Viewport } from 'next'
 import Layout from '@/components/Global/Layout'
 import * as components from '@/components'
+
+const app_title = 'Peanut Wallet'
 
 export const metadata: Metadata = {
     title: 'Peanut Protocol',
@@ -16,7 +18,21 @@ export const metadata: Metadata = {
             },
         ],
     },
+    applicationName: 'Peanut Wallet',
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'black',
+        title: app_title,
+    },
+    formatDetection: {
+        telephone: false,
+    },
 }
+
+export const viewport: Viewport = {
+    themeColor: '#FFFFFF',
+}
+
 export default function Home() {
     return (
         <Layout className="!mx-0 w-full !px-0 !pt-0 ">
