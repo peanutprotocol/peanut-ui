cb8b339b4c966c95cb62cf6cccff04ecbcf5f19d
Hugo Montenegro <h@hugomontenegro.com>
2024-10-09 15:38:23 +0100

    chore: copy update

--- src/components/Global/Banner/index.tsx
+++ src/components/Global/Banner/index.tsx
@@ -9,7 +9,7 @@ export function Banner() {
 
     return (
         <MarqueeWrapper backgroundColor="bg-purple-1" direction="left">
-            <span className="mx-4 text-sm font-semibold ">This feature is currently in beta.</span>
+            <span className="mx-4 text-sm font-semibold ">Beta feature - share your thoughts!</span>
         </MarqueeWrapper>
     )
 }
