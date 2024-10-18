c748da159c2f9b31f17a6a4e94d3c35f36eb2d21
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-18 09:20:43 +0200

    style: revert footer to old layout with icons

--- src/components/Global/Footer/index.tsx
+++ src/components/Global/Footer/index.tsx
@@ -1,39 +1,32 @@
 'use client'
-import React from 'react'
+
 import Link from 'next/link'
 
-import { LINKS, SOCIALS } from './consts'
+import * as _consts from './consts'
 
 const Footer = () => {
-
     return (
         <footer>
-            <div className="flex w-full flex-wrap items-center justify-center gap-4 border-t-2 border-n-1 bg-white px-4 py-2 py-4 pt-4 shadow ring-2 ring-white dark:border-white dark:bg-n-2 md:p-6 lg:justify-between">
-                <div className="flex justify-center gap-2">
-                    {LINKS.map((link) => {
+            <div className="flex w-full flex-col gap-2 border-t border-n-1 bg-white py-4 text-black dark:border-white dark:bg-n-2 sm:gap-4  sm:py-8">
+                <div className="flex justify-center gap-4">
+                    {_consts.SOCIALS.map((social) => {
                         return (
-                            <Link
-                                key={link.name}
-                                href={link.url}
-                                className="font-bold uppercase transition-colors last:mr-0 hover:text-violet-3 dark:text-white dark:hover:text-purple-1 md:mr-4"
-                            >
-                                {link.name}
+                            <Link key={social.name} href={social.url} target="_blank">
+                                <img src={social.logoSrc} className="h-6" alt="twitter" />
                             </Link>
                         )
                     })}
                 </div>
-
-                <div className="flex justify-center gap-4">
-                    {SOCIALS.map((social) => {
+                <div className="flex justify-center gap-2 ">
+                    {_consts.LINKS.map((link) => {
                         return (
                             <Link
-                                key={social.name}
-                                href={social.url}
+                                key={link.name}
+                                href={link.url}
                                 target="_blank"
                                 className="font-semibold uppercase transition-colors last:mr-0 hover:text-violet-3 dark:text-white dark:hover:text-purple-1 md:mr-4"
                             >
-                                {/* <img src={social.logoSrc} className="h-6" alt={social.name} /> */}
-                                {social.name}
+                                {link.name}
                             </Link>
                         )
                     })}
