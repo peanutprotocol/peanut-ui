f1bb57b32822964178d2c8349d32cea8f23e7889
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-18 18:40:30 +0200

    fix: card component shadow settings static

--- src/components/0_Bruddle/Card.tsx
+++ src/components/0_Bruddle/Card.tsx
@@ -8,20 +8,28 @@ interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
     color?: ShadowColor
 }
 
+const shadowClasses: Record<ShadowColor, Record<ShadowSize, string>> = {
+    primary: {
+        '4': 'shadow-primary-4',
+        '6': 'shadow-primary-6',
+        '8': 'shadow-primary-8',
+    },
+    secondary: {
+        '4': 'shadow-secondary-4',
+        '6': 'shadow-secondary-6',
+        '8': 'shadow-secondary-8',
+    },
+}
+
 export const Card = ({ children, className, shadowSize = '4', color = 'primary', ...props }: CardProps) => {
-    const shadowClass = `shadow-${color}-${shadowSize}`
+    const shadowClass = shadowClasses[color][shadowSize]
 
     return (
         <div
-            className={classNames(
-                `flex border border-n-1 bg-white p-4 dark:border-white dark:bg-n-1`,
-                shadowClass,
-                className
-            )}
+            className={classNames(`flex border border-n-1 p-4 dark:border-white dark:bg-n-1`, shadowClass, className)}
             {...props}
         >
             {children}
         </div>
     )
 }
-
