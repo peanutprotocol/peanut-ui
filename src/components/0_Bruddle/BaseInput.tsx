b1655858440870603e87e2eb52d04440acedb3e2
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-21 18:28:43 +0200

    fix: base input missing forward ref

--- src/components/0_Bruddle/BaseInput.tsx
+++ src/components/0_Bruddle/BaseInput.tsx
@@ -1,7 +1,10 @@
+import { forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-const BaseInput = ({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
-    return <input className={twMerge('input', className)} {...props} />
-}
+const BaseInput = forwardRef(
+    ({ className, ...props }: React.InputHTMLAttributes<HTMLInputElement>, ref: React.Ref<HTMLInputElement>) => {
+        return <input ref={ref} className={twMerge('input', className)} {...props} />
+    }
+)
 
 export default BaseInput
