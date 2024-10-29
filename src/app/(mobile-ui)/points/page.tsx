3e9822886ff20705d386a3744f26e1e60d2d4ba1
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-29 09:26:19 +0100

    feat: add points screen

---
+++ src/app/(mobile-ui)/points/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Icon from '@/components/Global/Icon'
+import { useAuth } from '@/context/authContext'
+
+const PointsPage = () => {
+    const { user } = useAuth()
+    const points = user?.totalPoints ?? 0
+    const invites = user?.referredUsers ?? 0
+
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-between">
+            <div className="flex flex-col gap-2">
+                <div>
+                    <div className="flex flex-row items-center">
+                        <Icon name="arrow-next" className="h-8 w-8" />
+                        {points} Points
+                    </div>
+                </div>
+                <div>
+                    <div className="flex flex-row items-center">
+                        <Icon name="arrow-next" className="h-8 w-8" />
+                        {invites} Invites
+                    </div>
+                </div>
+            </div>
+            <img src="/peanut-club.png" className="h-[240px] w-[240px]" alt="logo" />
+        </div>
+    )
+}
+
+export default PointsPage
--- src/components/Home/HomeNav.tsx
+++ src/components/Home/HomeNav.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from '@/context/authContext'
 import Icon from '../Global/Icon'
+import Link from 'next/link'
 
 const HomeNav = () => {
     const { user } = useAuth()
@@ -7,11 +8,13 @@ const HomeNav = () => {
     const invites = user?.referredUsers ?? 0
 
     return (
-        <div className="flex h-10 flex-row items-center justify-center gap-4 border-b-2 border-black bg-purple-1">
-            <span className="">{points} Points</span>
-            <Icon name="heart" />
-            <span className="">{invites} Invites</span>
-        </div>
+        <Link className="w-full" href={'/points'}>
+            <div className="flex h-10 flex-row items-center justify-center gap-4 border-b-2 border-black bg-purple-1">
+                <span className="">{points} Points</span>
+                <Icon name="heart" />
+                <span className="">{invites} Invites</span>
+            </div>
+        </Link>
     )
 }
 
