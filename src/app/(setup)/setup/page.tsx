47909702c01ed704ff81e36db8eeb0d57ee05b42
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-10-29 19:45:54 +0100

    ui-fix: update setup screen width on larger screens

--- src/app/(setup)/setup/page.tsx
+++ src/app/(setup)/setup/page.tsx
@@ -19,24 +19,26 @@ const SetupPage = () => {
     return (
         <div
             className={twMerge(
-                'flex h-screen flex-col gap-8 lg:gap-12 bg-opacity-100 p-6 transition-all',
+                'flex h-screen flex-col bg-opacity-100 p-6 transition-all',
                 step.containerClassname
             )}
         >
-            <div className="flex h-[100px] flex-col gap-4">
-                <h1 className="text-center text-5xl font-bold">{step.title}</h1>
-                <p className="text-center">{step.description}</p>
-            </div>
-            <div className="relative flex-grow flex flex-row items-center h-[50vh] justify-center">
-                {starPositions.map((positions, index) => (
-                    <img key={index} src={starImage.src} alt="Star" className={twMerge(positions, "absolute z-[11]")} />
-                ))}
-                <img src={peanutClub.src} className="z-10 object-contain h-full aspect-square" />
-            </div>
-            <div className="relative overflow-hidden">
-                <StepTransition step={currentStep} direction={direction}>
-                    <step.component />
-                </StepTransition>
+            <div className='w-full md:w-1/2 mg:1/3 mx-auto gap-8 lg:gap-12 h-full flex flex-col'>
+                <div className="flex h-[100px] flex-col gap-4">
+                    <h1 className="text-center text-5xl font-bold">{step.title}</h1>
+                    <p className="text-center">{step.description}</p>
+                </div>
+                <div className="relative flex-grow flex flex-row items-center h-[50vh] justify-center">
+                    {starPositions.map((positions, index) => (
+                        <img key={index} src={starImage.src} alt="Star" className={twMerge(positions, "absolute z-[11]")} />
+                    ))}
+                    <img src={peanutClub.src} className="z-10 object-contain h-full aspect-square" />
+                </div>
+                <div className="relative overflow-hidden">
+                    <StepTransition step={currentStep} direction={direction}>
+                        <step.component />
+                    </StepTransition>
+                </div>
             </div>
         </div >
     )
