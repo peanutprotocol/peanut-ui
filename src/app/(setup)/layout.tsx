2800c1a84558bc45ebb2e02310f76dee888ab082
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-11-12 21:58:56 +0100

    feat: don't show pwa setup screen when in PWA

--- src/app/(setup)/layout.tsx
+++ src/app/(setup)/layout.tsx
@@ -4,13 +4,18 @@ import '../../styles/globals.bruddle.css'
 import { useRouter } from 'next/navigation'
 import { SetupFlowProvider } from '../../components/Setup/context/SetupFlowContext'
 import { SETUP_STEPS } from '@/components/Setup/Setup.consts'
+import { usePWAStatus } from '@/hooks/usePWAStatus'
 
 const SetupLayout = ({ children }: { children?: React.ReactNode }) => {
     const { push } = useRouter()
+    const isPWA = usePWAStatus()
 
     return (
         <SetupFlowProvider
-            steps={SETUP_STEPS}
+            steps={SETUP_STEPS.filter((step) => {
+                // Remove pwa-install step if PWA is already installed
+                return step.screenId !== 'pwa-install' || !isPWA
+            })}
             onComplete={() => {
                 push('/home')
             }}
