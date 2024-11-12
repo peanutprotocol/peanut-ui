8e60d5fd20f03007da3d2ace7db4bb69825f494b
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2024-11-12 16:47:13 +0100

    feat: add pwa manifest & icons

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Peanut',
+        short_name: 'Peanut',
+        description: 'Butter smooth global money',
+        start_url: '/home',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+            {
+                src: '/icons/icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    }
+}
