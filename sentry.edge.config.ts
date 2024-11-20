96e291d3c2365283bb5d55ac1d109f097ee1e896
Hugo Montenegro <h@hugomontenegro.com>
2024-11-20 21:03:43 +0700

    chore: better logging

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,19 +9,21 @@ if (process.env.NODE_ENV !== 'development') {
     Sentry.init({
         dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
         enabled: true,
-        // Adjust this value in production, or use tracesSampler for greater control
         tracesSampleRate: 1,
-
-        // Setting this option to true will print useful information to the console while you're setting up Sentry.
         debug: false,
+        
+        beforeSend(event) {
+            if (event.request?.headers) {
+                delete event.request.headers['Authorization']
+                delete event.request.headers['api-key']
+            }
+            return event
+        },
+        
         integrations: [
-            // Sentry.replayIntegration({
-            //     // Additional Replay configuration goes in here, for example:
-            //     maskAllText: true,
-            //     blockAllMedia: true,
-            // }),
+            Sentry.httpIntegration(),
             Sentry.captureConsoleIntegration({
-                levels: ['error'],
+                levels: ['error', 'warn'],
             }),
         ],
     })
