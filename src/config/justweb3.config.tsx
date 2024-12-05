61dc51e7291f26ea76f315f20e3118f3c6a32cb0
Ghadi Mhawej <ghadimhawej@outlook.com>
2024-12-05 16:44:02 +0200

    fix: fix css module import

--- src/config/justweb3.config.tsx
+++ src/config/justweb3.config.tsx
@@ -1,42 +1,37 @@
 'use client'
-import { JustWeb3Provider as JustWeb3ProviderPrimitive, JustWeb3ProviderConfig } from '@justweb3/widget';
-import '@justweb3/widget/styles.css';
-import { ChainId } from '@justaname.id/sdk';
+import { JustWeb3Provider as JustWeb3ProviderPrimitive, JustWeb3ProviderConfig } from '@justweb3/widget'
+import '@justweb3/widget/styles.css'
+import { ChainId } from '@justaname.id/sdk'
 
-export const chainId = process.env.NEXT_PUBLIC_JUSTANAME_CHAIN_ID ? parseInt(process.env.NEXT_PUBLIC_JUSTANAME_CHAIN_ID) as ChainId : 1;
-export const peanutEnsDomain = process.env.NEXT_PUBLIC_JUSTANAME_ENS_DOMAIN || '';
+export const chainId = process.env.NEXT_PUBLIC_JUSTANAME_CHAIN_ID
+    ? (parseInt(process.env.NEXT_PUBLIC_JUSTANAME_CHAIN_ID) as ChainId)
+    : 1
+export const peanutEnsDomain = process.env.NEXT_PUBLIC_JUSTANAME_ENS_DOMAIN || ''
 export const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY
 const justweb3Config: JustWeb3ProviderConfig = {
     networks: [
         {
-            chainId:1,
+            chainId: 1,
             providerUrl: `https://mainnet.infura.io/v3/` + infuraApiKey,
         },
         {
-            chainId:11155111,
+            chainId: 11155111,
             providerUrl: `https://sepolia.infura.io/v3/` + infuraApiKey,
-        }
+        },
     ],
     ensDomains: [
         {
             ensDomain: peanutEnsDomain,
-            chainId:1
-        }
+            chainId: 1,
+        },
     ],
-    openOnWalletConnect:false,
-    color: {
-        primary: '#75b2d7'
-    }
+    openOnWalletConnect: false,
 }
 
-export interface JustaNameProviderConfig{
-    children: React.ReactNode;
+export interface JustaNameProviderConfig {
+    children: React.ReactNode
 }
 
-export const JustWeb3Provider: React.FC<JustaNameProviderConfig> = ({children}) => {
-    return (
-        <JustWeb3ProviderPrimitive config={justweb3Config}>
-            {children}
-        </JustWeb3ProviderPrimitive>
-    )
+export const JustWeb3Provider: React.FC<JustaNameProviderConfig> = ({ children }) => {
+    return <JustWeb3ProviderPrimitive config={justweb3Config}>{children}</JustWeb3ProviderPrimitive>
 }
