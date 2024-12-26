76c466bda8863f69d55c374447cdee0354b081d5
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-12-26 23:06:19 +0530

    feat: connect external wallet

--- src/components/Setup/Views/AddWallets.tsx
+++ src/components/Setup/Views/AddWallets.tsx
@@ -1,19 +1,26 @@
 import { Button } from '@/components/0_Bruddle'
 import { useSetupFlow } from '@/hooks/useSetupFlow'
+import { useAppKit } from '@reown/appkit/react'
+import { useAccount } from 'wagmi'
 
 const AddWallets = () => {
+    const { open } = useAppKit()
+    const { isConnected, isConnecting } = useAccount()
     const { handleNext } = useSetupFlow()
+
+    // todo: replace with new add-wallet component when ready
+    const handleWalletConnect = () => {
+        if (isConnected) {
+            handleNext()
+        } else {
+            open()
+        }
+    }
 
     return (
         <div className="flex h-full flex-col justify-end gap-2">
-            <Button
-                variant="dark"
-                disabled
-                onClick={() => {
-                    // Logic to connect a wallet & create a new account
-                }}
-            >
-                Connect
+            <Button variant="purple" disabled={isConnecting} onClick={handleWalletConnect} shadowSize="4">
+                {isConnected ? 'Next' : 'Connect'}
             </Button>
         </div>
     )
