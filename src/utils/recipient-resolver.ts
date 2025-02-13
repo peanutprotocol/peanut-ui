2f05890f9d3cf101dd0ac0648303678754da24a3
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-02-14 01:30:54 +0530

    feat: add recipient resolver

---
+++ src/utils/recipient-resolver.ts
@@ -0,0 +1,26 @@
+import { isAddress } from 'viem'
+import { resolveFromEnsName } from './general.utils'
+
+export type RecipientType = 'ENS' | 'ADDRESS' | 'USERNAME'
+
+export async function resolveRecipientToAddress(recipient: string): Promise<string> {
+    // if already an address, return it
+    if (isAddress(recipient)) {
+        return recipient
+    }
+
+    // check if its ENS
+    // TODO: add support for other ENS domains
+    if (recipient.endsWith('.eth')) {
+        const resolved = await resolveFromEnsName(recipient)
+        if (!resolved) {
+            throw new Error('Could not resolve ENS name')
+        }
+        return resolved
+    }
+
+    // try resolving as peanut username
+    // todo: add support for peanut username and justaname domains
+
+    return recipient
+}
