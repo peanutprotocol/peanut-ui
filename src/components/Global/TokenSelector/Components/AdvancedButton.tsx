d44e28f41e6b4078c9c0380c021c4f98ec30053b
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-02-25 16:28:21 +0530

    fix: zero balance shown on token selector when balance is being fetched

--- src/components/Global/TokenSelector/Components/AdvancedButton.tsx
+++ src/components/Global/TokenSelector/Components/AdvancedButton.tsx
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
-import Icon from '../../Icon'
+import * as context from '@/context'
+import { useBalance } from '@/hooks/useBalance'
 import * as utils from '@/utils'
 import { fetchTokenSymbol } from '@/utils'
-import * as context from '@/context'
 import peanut from '@squirrel-labs/peanut-sdk'
+import { useContext, useEffect, useState } from 'react'
 import { useAccount } from 'wagmi'
-import { useBalance } from '@/hooks/useBalance'
+import Icon from '../../Icon'
 import Loading from '../../Loading'
 
 interface IAdvancedTokenSelectorButtonProps {
@@ -44,20 +44,26 @@ export const AdvancedTokenSelectorButton = ({
     const { hasFetchedBalances } = useBalance()
     const [_tokenBalance, _setTokenBalance] = useState<number | undefined>(tokenBalance)
     const [_tokenSymbol, _setTokenSymbol] = useState<string | undefined>(tokenSymbol)
+    const [isFetchingTokenBalance, setIsFetchingTokenBalance] = useState<boolean>(false)
 
     const getTokenBalance = async () => {
-        const balance = Number(
-            await peanut.getTokenBalance({
-                chainId: selectedChainID,
-                tokenAddress: selectedTokenAddress,
-                walletAddress: address ?? '',
-            })
-        )
+        setIsFetchingTokenBalance(true)
+        try {
+            const balance = Number(
+                await peanut.getTokenBalance({
+                    chainId: selectedChainID,
+                    tokenAddress: selectedTokenAddress,
+                    walletAddress: address ?? '',
+                })
+            )
 
-        if (balance) {
-            _setTokenBalance(balance)
-        } else {
-            _setTokenBalance(tokenBalance)
+            if (balance) {
+                _setTokenBalance(balance)
+            } else {
+                _setTokenBalance(tokenBalance)
+            }
+        } finally {
+            setIsFetchingTokenBalance(false)
         }
     }
 
@@ -72,6 +78,8 @@ export const AdvancedTokenSelectorButton = ({
             getTokenBalance()
         }
     }, [tokenBalance, selectedChainID, selectedTokenAddress, address, isConnected])
+
+    console.log('_tokenBalance', _tokenBalance)
 
     useEffect(() => {
         let isMounted = true
@@ -133,9 +141,15 @@ export const AdvancedTokenSelectorButton = ({
                     {type === 'send' &&
                         (hasFetchedBalances ? (
                             <p className="text-xs text-gray-1">
-                                Balance: {utils.formatTokenAmount(_tokenBalance ?? 0, 4)}
+                                Balance:{' '}
+                                {isFetchingTokenBalance ? (
+                                    <Loading className="h-2 w-2" />
+                                ) : (
+                                    `
+                                    ${utils.formatTokenAmount(_tokenBalance ?? 0, 4)} ${tokenSymbol}`
+                                )}
                             </p>
-                        ) : address ? (
+                        ) : address || isFetchingTokenBalance ? (
                             <div className="flex flex-row items-center justify-center gap-1 text-xs text-gray-1">
                                 Balance: <Loading className="h-2 w-2" />
                             </div>
