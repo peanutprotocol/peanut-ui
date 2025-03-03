2db5f82b77aa73722d76b3a41a0e56257fe95fe5
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-03-03 23:12:03 +0530

    fix: bring back peanut gif :)

--- src/components/LandingPage/hero.tsx
+++ src/components/LandingPage/hero.tsx
@@ -1,4 +1,4 @@
-import { ButterySmoothGlobalMoneyBanner, HandThumbsUp, PeanutGuyGIF, Sparkle } from '@/assets'
+import { AboutPeanut, ButterySmoothGlobalMoney, HandThumbsUp, PeanutGuyGIF, Sparkle } from '@/assets'
 import { Stack } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
@@ -38,21 +38,30 @@ export function Hero({ heading, marquee = { visible: false }, cta, buttonVisible
             <CloudImages screenWidth={screenWidth} />
 
             <div className="lg:mb-16- lg:mt-24- relative mb-8 mt-12 flex grow flex-col justify-between space-y-6 md:mb-10 md:mt-12">
-                <img
-                    src={PeanutGuyGIF.src}
-                    className="mg:bottom-0 absolute bottom-4 left-1/2 z-10 mx-auto h-auto max-h-[60vh] w-auto max-w-[80%] -translate-x-1/2 transform object-contain md:hidden md:w-auto"
-                    alt="Peanut Guy"
-                />
-
-                {/* Title! */}
                 <Stack spacing={2} className="relative h-1/3 items-center justify-center px-4 text-center lg:h-full">
                     <img
-                        src={ButterySmoothGlobalMoneyBanner.src}
-                        className="z-0 mx-auto w-full max-w-[1000px] object-contain lg:w-3/4"
+                        src={ButterySmoothGlobalMoney.src}
+                        className="z-0 mx-auto w-full max-w-[1000px] object-contain lg:w-[40%]"
                         alt="Buttery Smooth Global Money"
                     />
 
                     <HeroImages />
+                </Stack>
+                <img
+                    src={PeanutGuyGIF.src}
+                    className="mg:bottom-0 mx-auto h-auto max-h-[60vh] w-[80%] transform object-contain md:w-auto lg:w-[20%]"
+                    alt="Peanut Guy"
+                />
+                <Stack spacing={2} className="relative h-1/3 items-center justify-center px-4 text-center lg:h-full">
+                    <img
+                        src={AboutPeanut.src}
+                        className="z-0 mx-auto w-full max-w-[1000px] object-contain lg:w-[40%]"
+                        alt="Buttery Smooth Global Money"
+                    />
+
+                    <div className="hidden md:block">
+                        <HeroImages />
+                    </div>
                 </Stack>
             </div>
 
