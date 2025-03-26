f0efa275a5f02cdcab04fa2d38e3896d5273776a
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-03-27 01:34:37 +0530

    feat: wallet icons

---
+++ src/assets/wallets/index.ts
@@ -0,0 +1,5 @@
+export { default as COINBASE_LOGO } from './coinbase.svg'
+export { default as METAMASK_LOGO } from './metamask.svg'
+export { default as RAINBOW_LOGO } from './rainbow.svg'
+export { default as TRUST_WALLET_LOGO } from './trust_wallet.svg'
+
