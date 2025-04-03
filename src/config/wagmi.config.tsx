f2a4a82c2b74ccf0681b2d3eb0a1448513d107ef
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-04-03 22:36:08 +0530

    fix: revert to favicon for appkit logo

--- src/config/wagmi.config.tsx
+++ src/config/wagmi.config.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { PEANUTMAN_LOGO } from '@/assets'
 import { JustaNameContext } from '@/config/justaname.config'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import {
@@ -29,7 +28,7 @@ const metadata = {
     name: 'Peanut Protocol',
     description: 'Peanut protocol - send crypto with links',
     url: process.env.NEXT_PUBLIC_BASE_URL || 'https://peanut.to', // origin must match your domain & subdomain
-    icons: [PEANUTMAN_LOGO],
+    icons: [`${process.env.NEXT_PUBLIC_BASE_URL}/favicon.ico`],
 }
 
 export const networks = [arbitrum, mainnet, optimism, polygon, gnosis, base, scroll, mantle, bsc] as [
