6b95a2815875311a41e905cc4e9376543c749e3e
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-04-10 01:24:45 +0530

    feat: update send flow views and use redux states

---
+++ src/components/Send/SendFlowManager.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { PEANUT_WALLET_CHAIN, PEANUT_WALLET_TOKEN } from '@/constants'
+import { tokenSelectorContext } from '@/context'
+import { useWallet } from '@/hooks/wallet/useWallet'
+import { useAppDispatch, useSendFlowStore } from '@/redux/hooks'
+import { sendFlowActions } from '@/redux/slices/send-flow-slice'
+import { fetchWithSentry } from '@/utils'
+import { useContext, useEffect } from 'react'
+import ConfirmSendView from './views/Confirm.send.view'
+import InitialSendView from './views/Initial.send.view'
+import SuccessSendView from './views/Success.send.view'
+
+const SendFlowManager = () => {
+    const dispatch = useAppDispatch()
+    const { view } = useSendFlowStore()
+    const { isPeanutWallet } = useWallet()
+    const { resetTokenContextProvider, setSelectedChainID, setSelectedTokenAddress } = useContext(tokenSelectorContext)
+
+    const fetchAndSetCrossChainDetails = async () => {
+        const response = await fetchWithSentry('https://apiplus.squidrouter.com/v2/chains', {
+            headers: {
+                'x-integrator-id': '11CBA45B-5EE9-4331-B146-48CCD7ED4C7C',
+            },
+        })
+        if (!response.ok) {
+            throw new Error('Squid: Network response was not ok')
+        }
+        const data = await response.json()
+
+        dispatch(sendFlowActions.setCrossChainDetails(data.chains))
+    }
+
+    useEffect(() => {
+        resetTokenContextProvider()
+        fetchAndSetCrossChainDetails()
+    }, [])
+
+    useEffect(() => {
+        if (isPeanutWallet) {
+            setSelectedChainID(PEANUT_WALLET_CHAIN.id.toString())
+            setSelectedTokenAddress(PEANUT_WALLET_TOKEN)
+        }
+    }, [isPeanutWallet])
+
+    return (
+        <div>
+            {view === 'INITIAL' && <InitialSendView />}
+            {view === 'CONFIRM' && <ConfirmSendView />}
+            {view === 'SUCCESS' && <SuccessSendView />}
+        </div>
+    )
+}
+
+export default SendFlowManager
