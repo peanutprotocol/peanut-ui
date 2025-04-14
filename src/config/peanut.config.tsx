bcc5066d7a330e7ac0b19fb593153a7d80e8b13e
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-04-15 01:58:14 +0530

    fix: revert reactGA dev env change

--- src/config/peanut.config.tsx
+++ src/config/peanut.config.tsx
@@ -16,9 +16,7 @@ import '../../sentry.server.config'
 
 export function PeanutProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        if (process.env.NODE_ENV !== 'development') {
-            ReactGA.initialize(process.env.NEXT_PUBLIC_GA_KEY ?? '')
-        }
+        ReactGA.initialize(process.env.NEXT_PUBLIC_GA_KEY ?? '')
         peanut.toggleVerbose(true)
         // LogRocket.init('x2zwq1/peanut-protocol')
         countries.registerLocale(enLocale)
