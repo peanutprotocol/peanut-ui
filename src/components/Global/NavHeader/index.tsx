b4149e1ea9f29c3f2bf560dfe5828e4687036705
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-04-15 15:35:34 +0530

    fix: navheader cta size

--- src/components/Global/NavHeader/index.tsx
+++ src/components/Global/NavHeader/index.tsx
@@ -11,8 +11,8 @@ const NavHeader = ({ title, href }: NavHeaderProps) => {
     return (
         <div className="relative flex w-full flex-row items-center justify-between md:block">
             <Link href={href ?? '/home'} className="md:hidden">
-                <Button variant="stroke" className="h-6 w-6 p-0">
-                    <Icon name="arrow-prev" className="h-4 w-4" />
+                <Button variant="stroke" className="h-7 w-7 p-0">
+                    <Icon name="arrow-prev" className="h-5 w-5" />
                 </Button>
             </Link>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform pb-1 text-lg font-bold md:relative md:left-auto md:top-auto md:translate-x-0 md:translate-y-0 md:transform-none md:pb-0 md:text-base md:font-semibold">
