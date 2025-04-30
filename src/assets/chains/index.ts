1b320a401d835be71608df6a8ff6aecd33ae5ba5
Kushagra Sarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-04-30 22:40:34 +0530

    Update src/assets/chains/index.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/assets/chains/index.ts
+++ src/assets/chains/index.ts
@@ -1,5 +1,5 @@
 export { default as ARBITRUM_ICON } from './arbitrum.svg'
 export { default as MANTLE_ICON } from './mantle.svg'
-export { default as TRON_ICON } from './solana.svg'
-export { default as SOLANA_ICON } from './tron.svg'
+export { default as SOLANA_ICON } from './solana.svg'
+export { default as TRON_ICON } from './tron.svg'
 
