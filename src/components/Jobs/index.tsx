ad55c7eeb1d813628f9cd6f57c2c419a4b9395fb
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-05-09 00:30:45 +0530

    fix: public pages alignment

--- src/components/Jobs/index.tsx
+++ src/components/Jobs/index.tsx
@@ -2,11 +2,11 @@ import { PeanutGuyGIF } from '@/assets'
 
 export function Careers() {
     return (
-        <div className="flex h-full flex-col-reverse items-center lg:flex-row">
-            <div className="-ml-16 w-4/5 md:w-1/2">
+        <div className="flex h-full flex-col-reverse items-center justify-center lg:flex-row">
+            <div className="w-4/5 md:w-1/2">
                 <img src={PeanutGuyGIF.src} className="h-full w-auto md:h-fit md:w-fit" />
             </div>
-            <div className="my-16 flex w-3/4 flex-col justify-center gap-0 self-center md:my-32 md:px-8 lg:self-auto">
+            <div>
                 <div className="font-display text-xl lg:text-3xl">{'<'} Hey there! Want to work at Peanut?</div>
 
                 <div className="mt-4 text-lg">
