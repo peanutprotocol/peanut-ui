22677de4361bcedbb8edc8bac17271eb9d5af6af
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-05-13 20:27:31 +0530

    fix: cache keys

--- next.config.js
+++ next.config.js
@@ -107,23 +107,6 @@ const nextConfig = {
                     },
                 ],
             },
-            {
-                source: '/',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-                    },
-                    {
-                        key: 'Pragma',
-                        value: 'no-cache',
-                    },
-                    {
-                        key: 'Expires',
-                        value: '0',
-                    },
-                ],
-            },
         ]
     },
 }
