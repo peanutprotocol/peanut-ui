743cad76d94cf30decd859c7b544f15406e70c0f
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-05-16 01:08:50 +0530

    feat: replace jobs with careers

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -3,13 +3,13 @@ import Layout from '@/components/Global/Layout'
 import { generateMetadata } from '../metadata'
 
 export const metadata = generateMetadata({
-    title: 'Jobs | Peanut',
+    title: 'Careers | Peanut',
     description: 'Join the Peanut team and help us build the future of crypto payments.',
     image: '/metadata-img.png',
     keywords: 'jobs, careers, work, employment, crypto, payments',
 })
 
-export default function JobsPage() {
+export default function CareersPage() {
     return (
         <Layout>
             <Jobs />
