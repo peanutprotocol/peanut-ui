6ebb786c55fee9d8d4980e7b154f9d69567fbec6
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-06-06 22:19:28 +0530

    fix: remove passkey link from setup step

--- src/components/Setup/Views/SetupPasskey.tsx
+++ src/components/Setup/Views/SetupPasskey.tsx
@@ -6,7 +6,6 @@ import { WalletProviderType } from '@/interfaces'
 import { useAppDispatch, useSetupStore } from '@/redux/hooks'
 import { setupActions } from '@/redux/slices/setup-slice'
 import { getFromLocalStorage } from '@/utils'
-import { Link } from '@chakra-ui/react'
 import * as Sentry from '@sentry/nextjs'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
@@ -51,41 +50,27 @@ const SetupPasskey = () => {
 
     return (
         <div>
-            <div className="flex h-full min-h-32 flex-col justify-between p-0 md:min-h-32">
-                <div className="flex h-full flex-col justify-end gap-2 text-center">
-                    <Button
-                        loading={isLoading}
-                        disabled={isLoading}
-                        onClick={async () => {
-                            dispatch(setupActions.setLoading(true))
-                            try {
-                                await handleRegister(username)
-                            } catch (e) {
-                                Sentry.captureException(e)
-                                console.error('Error registering passkey:', e)
-                                setError('Error registering passkey.')
-                                dispatch(setupActions.setLoading(false))
-                            }
-                        }}
-                        className="text-nowrap"
-                        shadowSize="4"
-                    >
-                        Set it up
-                    </Button>
-                    {error && <p className="text-sm font-bold text-error">{error}</p>}
-                </div>
-                <div>
-                    <p className="border-t border-grey-1 pt-2 text-center text-xs text-grey-1">
-                        <Link
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className="underline underline-offset-2"
-                            href="https://peanutprotocol.notion.site/Terms-of-Service-Privacy-Policy-1f245331837f4b7e860261be8374cc3a?pvs=4"
-                        >
-                            Learn more about what Passkeys are
-                        </Link>{' '}
-                    </p>
-                </div>
+            <div className="flex h-full flex-col justify-between gap-3 p-0 ">
+                <Button
+                    loading={isLoading}
+                    disabled={isLoading}
+                    onClick={async () => {
+                        dispatch(setupActions.setLoading(true))
+                        try {
+                            await handleRegister(username)
+                        } catch (e) {
+                            Sentry.captureException(e)
+                            console.error('Error registering passkey:', e)
+                            setError('Error registering passkey.')
+                            dispatch(setupActions.setLoading(false))
+                        }
+                    }}
+                    className="text-nowrap"
+                    shadowSize="4"
+                >
+                    Set it up
+                </Button>
+                {error && <p className="text-sm font-bold text-error">{error}</p>}
             </div>
         </div>
     )
