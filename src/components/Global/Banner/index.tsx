7d038cb66609cfdf24292e95ff756adf56101712
Hugo Montenegro <hugo@peanut.to>
2025-06-18 12:42:45 +0100

    Update index.tsx

--- src/components/Global/Banner/index.tsx
+++ src/components/Global/Banner/index.tsx
@@ -6,7 +6,7 @@ export function Banner() {
     if (pathname === '/cashout') return null
     return (
         <GenericBanner
-            message="New product out! Try peanut.me now and earn rewards on our Beta program."
+            message="New product out! Try peanut.me now and earn rewards on our Beta program"
             backgroundColor="bg-purple-1"
         />
     )
