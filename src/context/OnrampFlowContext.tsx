9bb1fd0599ca56434601d613581ae66ddb3f0bf7
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-06-27 21:58:58 +0200

    fix: build error

--- src/context/OnrampFlowContext.tsx
+++ src/context/OnrampFlowContext.tsx
@@ -54,7 +54,7 @@ export const OnrampFlowContextProvider: React.FC<{ children: ReactNode }> = ({ c
             setFromBankSelected,
             resetOnrampFlow,
         }),
-        [amountToOnramp, currentView, error, fromBankSelected, resetFlow]
+        [amountToOnramp, currentView, error, fromBankSelected, resetOnrampFlow]
     )
 
     return <OnrampFlowContext.Provider value={value}>{children}</OnrampFlowContext.Provider>
