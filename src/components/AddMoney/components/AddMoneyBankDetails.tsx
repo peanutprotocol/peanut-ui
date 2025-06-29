100b862ecb81366c3e557fa4c5076fe2c1e211e5
Facundo Bozzi <facundo.bo@hotmail.com>
2025-06-29 17:17:20 -0300

    fixed double import

--- src/components/AddMoney/components/AddMoneyBankDetails.tsx
+++ src/components/AddMoney/components/AddMoneyBankDetails.tsx
@@ -12,7 +12,6 @@ import { useEffect, useState, useMemo } from 'react'
 import { countryCodeMap, countryData } from '@/components/AddMoney/consts'
 import { formatCurrencyAmount } from '@/utils/currency'
 import { formatBankAccountDisplay } from '@/utils/format.utils'
-import { getOnrampCurrencyConfig, getCurrencySymbol } from '@/utils/bridge.utils'
 import Icon from '@/components/Global/Icon'
 import { getCurrencySymbol, getOnrampCurrencyConfig } from '@/utils/bridge.utils'
 
