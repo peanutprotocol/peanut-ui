839e91e5a022d6a887a449a6ef5e32094b3b701f
Facundo Bozzi <facundo.bo@hotmail.com>
2025-07-09 18:30:48 -0300

    fixed mgiration banner

--- src/components/Global/MigrationBanner/index.tsx
+++ src/components/Global/MigrationBanner/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { MarqueeWrapper } from '../MarqueeWrapper'
+import { PEANUTMAN_CHEERING } from '@/assets'
 
 interface MigrationBannerProps {
     className?: string
@@ -8,12 +9,11 @@ interface MigrationBannerProps {
 
 export function MigrationBanner({ className = '' }: MigrationBannerProps) {
     return (
-        // <div className={`relative w-full bg-yellow-1 text-black ${className}`}>
         <MarqueeWrapper backgroundColor="bg-yellow-1" direction="left">
-            <div className="flex items-center justify-center py-4">
-                <span className="text-h2 font-black uppercase tracking-wider">WE'VE MIGRATED TO PEANUT.ME</span>
+            <div className="mx-4 flex items-center py-4">
+                <img src={PEANUTMAN_CHEERING.src} alt="Migration Icon" className="mr-3 h-8 w-8" />
+                <span className="text-h2 font-bold uppercase tracking-wider">WE'VE MIGRATED TO PEANUT.ME</span>
             </div>
         </MarqueeWrapper>
-        // </div>
     )
 }
