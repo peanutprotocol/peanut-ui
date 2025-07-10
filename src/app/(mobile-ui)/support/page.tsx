2fe9903cb926ec02085b38d9bbc2beb5a4e8b89d
Kushagra Sarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-07-10 21:26:37 +0530

    fix: support page prod (#971)
    * fix: update desktop navigation
    
    * fix: support page

--- src/app/(mobile-ui)/support/page.tsx
+++ src/app/(mobile-ui)/support/page.tsx
@@ -4,7 +4,7 @@ const SupportPage = () => {
     return (
         <iframe
             src="https://go.crisp.chat/chat/embed/?website_id=916078be-a6af-4696-82cb-bc08d43d9125"
-            className="h-full w-full"
+            className="h-full w-full md:max-w-[90%] md:pl-24"
         />
     )
 }
