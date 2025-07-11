11e41f90390cfabd637385d60bad9ea625638e12
Facundo Bozzi <facundo.bo@hotmail.com>
2025-07-11 12:56:28 -0300

    animated thumbs up

--- src/components/Global/MigrationBanner/index.tsx
+++ src/components/Global/MigrationBanner/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { MarqueeWrapper } from '../MarqueeWrapper'
 import { PEANUTMAN_CHEERING } from '@/assets'
+import handThumbsUp from '@/assets/illustrations/hand-thumbs-up.svg'
 
 interface MigrationBannerProps {
     className?: string
@@ -11,7 +12,7 @@ export function MigrationBanner({ className = '' }: MigrationBannerProps) {
     return (
         <MarqueeWrapper backgroundColor="bg-yellow-1" direction="left">
             <div className={`mx-4 flex items-center py-4 ${className}`}>
-                <img src={PEANUTMAN_CHEERING.src} alt="Migration Icon" className="mr-3 h-8 w-8" />
+                <img src={handThumbsUp.src} alt="Migration Icon" className="animation-thumbsUp mr-3 h-8 w-8" />
                 <span className="text-h2 font-bold uppercase tracking-wider">WE'VE MIGRATED TO PEANUT.ME</span>
             </div>
         </MarqueeWrapper>
