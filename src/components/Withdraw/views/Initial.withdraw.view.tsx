162d37ddcbef05c5eb1f53bad16c179d13a3bf07
Facundo Bozzi <facundo.bo@hotmail.com>
2025-07-11 12:02:10 -0300

    deleting console.log

--- src/components/Withdraw/views/Initial.withdraw.view.tsx
+++ src/components/Withdraw/views/Initial.withdraw.view.tsx
@@ -93,7 +93,6 @@ export default function InitialWithdrawView({ amount, onReview, onBack, isProces
                     placeholder="Enter an address or ENS"
                     recipient={recipient}
                     onUpdate={(update: GeneralRecipientUpdate) => {
-                        console.log('isValid', update.isValid, update.errorMessage)
                         setRecipient(update.recipient)
                         setIsValidRecipient(update.isValid)
                         setError({
