f2b1134d8d375d3a14d66a7644edfe5360e26df4
Mohd Zishan <72738005+Zishan-7@users.noreply.github.com>
2025-07-31 17:56:46 +0530

    copy changes for hotfix from prod (#1042)

--- src/app/(mobile-ui)/add-money/crypto/page.tsx
+++ src/app/(mobile-ui)/add-money/crypto/page.tsx
@@ -16,8 +16,8 @@ type AddMoneyCryptoStep = 'sourceSelection' | 'tokenSelection' | 'networkSelecti
 const AddMoneyCryptoPage = () => {
     const router = useRouter()
     const { address: peanutWalletAddress } = useWallet()
-    const [currentStep, setCurrentStep] = useState<AddMoneyCryptoStep>('sourceSelection')
-    const [selectedSource, setSelectedSource] = useState<CryptoSource | null>(null)
+    const [currentStep, setCurrentStep] = useState<AddMoneyCryptoStep>('tokenSelection') // hotfix for deposit - select tokenSelection view as default
+    const [selectedSource, setSelectedSource] = useState<CryptoSource | null>(CRYPTO_EXCHANGES[3]) // hotfix for deposit - select Other exhange by default
     const [selectedToken, setSelectedToken] = useState<CryptoToken | null>(null)
     const [selectedNetwork, setSelectedNetwork] = useState<SelectedNetwork | null>(null)
     const [isRiskAccepted, setIsRiskAccepted] = useState(false)
@@ -51,6 +51,11 @@ const AddMoneyCryptoPage = () => {
     }
 
     const handleBackToSourceSelection = () => {
+        // hotfix for deposit - redirect to previous route if user is on tokenSelection and selected source is other-exchanges
+        if (selectedSource?.id === 'other-exchanges' && currentStep === 'tokenSelection') {
+            router.back()
+            return
+        }
         setCurrentStep('sourceSelection')
         setSelectedSource(null)
         resetSelections()
@@ -69,7 +74,7 @@ const AddMoneyCryptoPage = () => {
 
     const handleBackToNetworkSelectionFromQR = () => {
         if (selectedSource?.type === 'exchange') {
-            setCurrentStep('sourceSelection')
+            setCurrentStep('tokenSelection') // hotfix for deposit - redirect to tokenSelection view if user is on qrScreen and selected source is exchange
         } else {
             setCurrentStep('networkSelection')
         }
