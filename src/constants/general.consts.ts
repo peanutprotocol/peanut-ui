705914cca0cd8021a0982405c5bbf958f6832107
Juan José Ramírez <artjjrn@gmail.com>
2025-07-31 16:49:14 -0300

    fix(cross-chain): improve route estimation
    - Use linear interpolation to set realistic bounds

--- src/constants/general.consts.ts
+++ src/constants/general.consts.ts
@@ -8,9 +8,8 @@ export const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY
 export const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
 
 export const SQUID_INTEGRATOR_ID = process.env.SQUID_INTEGRATOR_ID!
-export const DEFAULT_SQUID_INTEGRATOR_ID =
-    process.env.DEFAULT_SQUID_INTEGRATOR_ID || process.env.NEXT_PUBLIC_DEFAULT_SQUID_INTEGRATOR_ID
-export const SQUID_API_URL = process.env.NEXT_PUBLIC_SQUID_API_URL
+export const SQUID_INTEGRATOR_ID_WITHOUT_CORAL = process.env.DEFAULT_SQUID_INTEGRATOR_ID!
+export const SQUID_API_URL = process.env.SQUID_API_URL!
 
 const infuraUrl = (subdomain: string) => (INFURA_API_KEY ? `https://${subdomain}.infura.io/v3/${INFURA_API_KEY}` : null)
 const alchemyUrl = (subdomain: string) =>
