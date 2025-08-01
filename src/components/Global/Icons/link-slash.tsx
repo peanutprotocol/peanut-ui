806d8b1193a0ccdee829210cec6dccd68923c224
Juan José Ramírez <artjjrn@gmail.com>
2025-08-01 18:14:31 -0300

    fix: typo

--- src/components/Global/Icons/link-slash.tsx
+++ src/components/Global/Icons/link-slash.tsx
@@ -1,6 +1,6 @@
 import { FC, SVGProps } from 'react'
 
-export const LinkSashIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
+export const LinkSlashIcon: FC<SVGProps<SVGSVGElement>> = (props) => {
     return (
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
             <path
