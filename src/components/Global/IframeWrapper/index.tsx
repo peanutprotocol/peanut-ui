0f45b5473f793945bb2bb5022a8df032cc521e3b
Zishan Mohd <zishan2539@gmail.com>
2025-08-27 21:32:16 +0530

    fix: pointer events

--- src/components/Global/IframeWrapper/index.tsx
+++ src/components/Global/IframeWrapper/index.tsx
@@ -1,5 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import Modal from '../Modal'
+import { Button, ButtonVariant } from '@/components/0_Bruddle'
+import { Icon, IconName } from '../Icons/Icon'
+import ActionModal from '../ActionModal'
+import { useRouter } from 'next/navigation'
+import StartVerificationView from './StartVerificationView'
 
 export type IFrameWrapperProps = {
     src: string
@@ -11,6 +16,72 @@ export type IFrameWrapperProps = {
 const IframeWrapper = ({ src, visible, onClose, closeConfirmMessage }: IFrameWrapperProps) => {
     const enableConfirmationPrompt = closeConfirmMessage !== undefined
     const [showCloseConfirmMessage, setShowCloseConfirmMessage] = useState(false)
+    const [isHelpModalOpen, setIsHelpModalOpen] = useState(false)
+    const [modalVariant, setModalVariant] = useState<'stop-verification' | 'trouble'>('trouble')
+    const [copied, setCopied] = useState(false)
+    const [isVerificationStarted, setIsVerificationStarted] = useState(false)
+    const router = useRouter()
+
+    const handleCopy = (textToCopy: string) => {
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            setCopied(true)
+            setTimeout(() => setCopied(false), 2000)
+        })
+    }
+
+    const modalDetails = useMemo(() => {
+        if (modalVariant === 'trouble') {
+            return {
+                title: 'Having trouble verifying?',
+                description:
+                    'If the verification isn’t loading here, you can finish it in your browser. Just copy this link and open it there.',
+                icon: 'info' as IconName,
+                iconContainerClassName: 'bg-primary-1',
+                ctas: [
+                    {
+                        text: 'Copy link',
+                        icon: copied ? 'check' : ('copy' as IconName),
+                        onClick: () => {
+                            handleCopy(src)
+                        },
+                        variant: 'purple' as ButtonVariant,
+                        shadowSize: '4' as const,
+                    },
+                    {
+                        text: 'Chat with support',
+                        icon: 'peanut-support' as IconName,
+                        onClick: () => router.push('/support'),
+                        variant: 'transparent' as ButtonVariant,
+                        className: 'underline text-sm font-medium w-full fill-none',
+                    },
+                ],
+            }
+        }
+
+        return {
+            title: 'Stop verification?',
+            description: 'If you exit now, your verification won’t be completed and you’ll need to start again later.',
+            icon: 'info' as IconName,
+            iconContainerClassName: 'bg-secondary-1',
+            ctas: [
+                {
+                    text: 'Stop verification',
+                    onClick: () => {
+                        setIsHelpModalOpen(false)
+                        onClose('manual')
+                    },
+                    variant: 'purple' as ButtonVariant,
+                    shadowSize: '4' as const,
+                },
+                {
+                    text: 'Continue verifying',
+                    onClick: () => setIsHelpModalOpen(false),
+                    variant: 'transparent' as ButtonVariant,
+                    className: 'underline text-sm font-medium w-full',
+                },
+            ],
+        }
+    }, [modalVariant, copied])
 
     // Reset showCloseConfirmMessage when visibility changes or src changes
     useEffect(() => {
@@ -38,79 +109,126 @@ const IframeWrapper = ({ src, visible, onClose, closeConfirmMessage }: IFrameWra
     }, [onClose])
 
     return (
-        <Modal
-            visible={visible}
-            onClose={() => {
-                if (!enableConfirmationPrompt) {
-                    onClose('manual')
-                    return
-                }
-                if (src.includes('tos')) {
-                    onClose('manual')
-                    return
-                }
-                setShowCloseConfirmMessage(true)
-            }}
-            classWrap="h-full w-full !max-w-none sm:!max-w-[600px] border-none sm:m-auto m-0"
-            classOverlay="bg-black bg-opacity-50"
-            video={false}
-            className="z-[1000001] !p-0 md:!p-6"
-            classButtonClose="hidden"
-            hideOverlay={false}
-        >
-            <div className="flex h-full flex-col gap-2 p-0">
-                <div className="w-full flex-shrink-0">
-                    {showCloseConfirmMessage ? (
-                        <div className="flex w-full flex-col justify-between gap-3 border-b border-n-1 p-4 md:h-14 md:flex-row md:items-center">
-                            <p className="text-sm">{closeConfirmMessage}</p>
-                            <div className="flex flex-row items-center gap-2">
+        <>
+            <Modal
+                visible={visible}
+                onClose={() => {
+                    if (!enableConfirmationPrompt) {
+                        onClose('manual')
+                        return
+                    }
+                    if (src.includes('tos')) {
+                        onClose('manual')
+                        return
+                    }
+                    setShowCloseConfirmMessage(true)
+                }}
+                classWrap="h-full w-full !max-w-none sm:!max-w-[600px] border-none sm:m-auto m-0"
+                classOverlay={`bg-black bg-opacity-50 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
+                video={false}
+                className={`z-[100] !p-0 md:!p-6 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
+                classButtonClose="hidden"
+                preventClose={true}
+                hideOverlay={false}
+            >
+                {!isVerificationStarted ? (
+                    <StartVerificationView onStartVerification={() => setIsVerificationStarted(true)} />
+                ) : (
+                    <div className="flex h-full flex-col gap-2 p-0">
+                        <div className="w-full flex-shrink-0">
+                            {showCloseConfirmMessage ? (
+                                <div className="flex w-full flex-col justify-between gap-3 border-b border-n-1 p-4 md:h-14 md:flex-row md:items-center">
+                                    <p className="text-sm">{closeConfirmMessage}</p>
+                                    <div className="flex flex-row items-center gap-2">
+                                        <button
+                                            className="btn-stroke h-10"
+                                            onClick={() => {
+                                                setShowCloseConfirmMessage(false)
+                                            }}
+                                        >
+                                            Cancel
+                                        </button>
+                                        <button
+                                            className="btn-purple h-10"
+                                            onClick={() => {
+                                                onClose('manual')
+                                                setShowCloseConfirmMessage(false)
+                                            }}
+                                        >
+                                            Close
+                                        </button>
+                                    </div>
+                                </div>
+                            ) : (
                                 <button
-                                    className="btn-stroke h-10"
+                                    className="btn-purple h-14 w-full rounded-none border-b border-n-1"
                                     onClick={() => {
-                                        setShowCloseConfirmMessage(false)
+                                        // only show confirmation for kyc step, otherwise close immediately
+                                        if (enableConfirmationPrompt && !src.includes('tos')) {
+                                            setShowCloseConfirmMessage(true)
+                                        } else {
+                                            onClose('manual')
+                                        }
                                     }}
                                 >
-                                    Cancel
+                                    CLOSE
                                 </button>
-                                <button
-                                    className="btn-purple h-10"
+                            )}
+                        </div>
+                        <div className="h-full w-full flex-grow overflow-scroll">
+                            <iframe
+                                key={src}
+                                src={src}
+                                allow="camera;"
+                                style={{ width: '100%', height: '80%', border: 'none' }}
+                                className="rounded-md"
+                                sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation"
+                            />
+                            <div className="flex h-1/5 w-full flex-col items-center justify-center gap-4 px-5">
+                                <Button
+                                    variant={'purple'}
+                                    className={`max-w-md`}
                                     onClick={() => {
-                                        onClose('manual')
-                                        setShowCloseConfirmMessage(false)
+                                        setModalVariant('stop-verification')
+                                        setIsHelpModalOpen(true)
                                     }}
+                                    shadowType="primary"
+                                    shadowSize="4"
                                 >
-                                    Close
-                                </button>
+                                    Stop verification process
+                                </Button>
+
+                                <span
+                                    onClick={() => {
+                                        setModalVariant('trouble')
+                                        setIsHelpModalOpen(true)
+                                    }}
+                                    className="flex items-center gap-1"
+                                >
+                                    <Icon name="peanut-support" size={16} />
+                                    <p className="text-xs font-medium underline">Having trouble?</p>
+                                </span>
                             </div>
                         </div>
-                    ) : (
-                        <button
-                            className="btn-purple h-14 w-full rounded-none border-b border-n-1"
-                            onClick={() => {
-                                // only show confirmation for kyc step, otherwise close immediately
-                                if (enableConfirmationPrompt && !src.includes('tos')) {
-                                    setShowCloseConfirmMessage(true)
-                                } else {
-                                    onClose('manual')
-                                }
-                            }}
-                        >
-                            CLOSE
-                        </button>
-                    )}
-                </div>
-                <div className="h-full w-full flex-grow overflow-scroll">
-                    <iframe
-                        key={src}
-                        src={src}
-                        allow="camera;"
-                        style={{ width: '100%', height: '100%', border: 'none' }}
-                        className="rounded-md"
-                        sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation"
-                    />
-                </div>
-            </div>
-        </Modal>
+                    </div>
+                )}
+            </Modal>
+            {isHelpModalOpen && (
+                <ActionModal
+                    visible={isHelpModalOpen}
+                    onClose={() => setIsHelpModalOpen(false)}
+                    title={modalDetails.title}
+                    description={modalDetails.description}
+                    icon={modalDetails.icon}
+                    iconContainerClassName={modalDetails.iconContainerClassName}
+                    modalPanelClassName="max-w-full pointer-events-auto"
+                    ctaClassName="grid grid-cols-1 gap-3"
+                    modalClassName="!z-[10001] pointer-events-auto"
+                    preventClose={true}
+                    ctas={modalDetails.ctas}
+                />
+            )}
+        </>
     )
 }
 
