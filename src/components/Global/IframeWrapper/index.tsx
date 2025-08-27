ccf384d6363ab00eea9e2a5f9a98b2217770dbd1
Zishan Mohd <zishan2539@gmail.com>
2025-08-27 21:45:51 +0530

    fix: modal btns not working on mobile

--- src/components/Global/IframeWrapper/index.tsx
+++ src/components/Global/IframeWrapper/index.tsx
@@ -109,126 +109,124 @@ const IframeWrapper = ({ src, visible, onClose, closeConfirmMessage }: IFrameWra
     }, [onClose])
 
     return (
-        <>
-            <Modal
-                visible={visible}
-                onClose={() => {
-                    if (!enableConfirmationPrompt) {
-                        onClose('manual')
-                        return
-                    }
-                    if (src.includes('tos')) {
-                        onClose('manual')
-                        return
-                    }
-                    setShowCloseConfirmMessage(true)
-                }}
-                classWrap="h-full w-full !max-w-none sm:!max-w-[600px] border-none sm:m-auto m-0"
-                classOverlay={`bg-black bg-opacity-50 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
-                video={false}
-                className={`z-[100] !p-0 md:!p-6 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
-                classButtonClose="hidden"
-                preventClose={true}
-                hideOverlay={false}
-            >
-                {!isVerificationStarted ? (
-                    <StartVerificationView onStartVerification={() => setIsVerificationStarted(true)} />
-                ) : (
-                    <div className="flex h-full flex-col gap-2 p-0">
-                        <div className="w-full flex-shrink-0">
-                            {showCloseConfirmMessage ? (
-                                <div className="flex w-full flex-col justify-between gap-3 border-b border-n-1 p-4 md:h-14 md:flex-row md:items-center">
-                                    <p className="text-sm">{closeConfirmMessage}</p>
-                                    <div className="flex flex-row items-center gap-2">
-                                        <button
-                                            className="btn-stroke h-10"
-                                            onClick={() => {
-                                                setShowCloseConfirmMessage(false)
-                                            }}
-                                        >
-                                            Cancel
-                                        </button>
-                                        <button
-                                            className="btn-purple h-10"
-                                            onClick={() => {
-                                                onClose('manual')
-                                                setShowCloseConfirmMessage(false)
-                                            }}
-                                        >
-                                            Close
-                                        </button>
-                                    </div>
-                                </div>
-                            ) : (
-                                <button
-                                    className="btn-purple h-14 w-full rounded-none border-b border-n-1"
-                                    onClick={() => {
-                                        // only show confirmation for kyc step, otherwise close immediately
-                                        if (enableConfirmationPrompt && !src.includes('tos')) {
-                                            setShowCloseConfirmMessage(true)
-                                        } else {
+        <Modal
+            visible={visible}
+            onClose={() => {
+                if (!enableConfirmationPrompt) {
+                    onClose('manual')
+                    return
+                }
+                if (src.includes('tos')) {
+                    onClose('manual')
+                    return
+                }
+                setShowCloseConfirmMessage(true)
+            }}
+            classWrap="h-full w-full !max-w-none sm:!max-w-[600px] border-none sm:m-auto m-0"
+            classOverlay={`bg-black bg-opacity-50 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
+            video={false}
+            className={`z-[100] !p-0 md:!p-6 ${isHelpModalOpen ? 'pointer-events-none' : ''}`}
+            classButtonClose="hidden"
+            preventClose={true}
+            hideOverlay={false}
+        >
+            {!isVerificationStarted ? (
+                <StartVerificationView onStartVerification={() => setIsVerificationStarted(true)} />
+            ) : (
+                <div className="flex h-full flex-col gap-2 p-0">
+                    <div className="w-full flex-shrink-0">
+                        {showCloseConfirmMessage ? (
+                            <div className="flex w-full flex-col justify-between gap-3 border-b border-n-1 p-4 md:h-14 md:flex-row md:items-center">
+                                <p className="text-sm">{closeConfirmMessage}</p>
+                                <div className="flex flex-row items-center gap-2">
+                                    <button
+                                        className="btn-stroke h-10"
+                                        onClick={() => {
+                                            setShowCloseConfirmMessage(false)
+                                        }}
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button
+                                        className="btn-purple h-10"
+                                        onClick={() => {
                                             onClose('manual')
-                                        }
-                                    }}
-                                >
-                                    CLOSE
-                                </button>
-                            )}
-                        </div>
-                        <div className="h-full w-full flex-grow overflow-scroll">
-                            <iframe
-                                key={src}
-                                src={src}
-                                allow="camera;"
-                                style={{ width: '100%', height: '80%', border: 'none' }}
-                                className="rounded-md"
-                                sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation"
-                            />
-                            <div className="flex h-1/5 w-full flex-col items-center justify-center gap-4 px-5">
-                                <Button
-                                    variant={'purple'}
-                                    className={`max-w-md`}
-                                    onClick={() => {
-                                        setModalVariant('stop-verification')
-                                        setIsHelpModalOpen(true)
-                                    }}
-                                    shadowType="primary"
-                                    shadowSize="4"
-                                >
-                                    Stop verification process
-                                </Button>
-
-                                <span
-                                    onClick={() => {
-                                        setModalVariant('trouble')
-                                        setIsHelpModalOpen(true)
-                                    }}
-                                    className="flex items-center gap-1"
-                                >
-                                    <Icon name="peanut-support" size={16} />
-                                    <p className="text-xs font-medium underline">Having trouble?</p>
-                                </span>
+                                            setShowCloseConfirmMessage(false)
+                                        }}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
                             </div>
+                        ) : (
+                            <button
+                                className="btn-purple h-14 w-full rounded-none border-b border-n-1"
+                                onClick={() => {
+                                    // only show confirmation for kyc step, otherwise close immediately
+                                    if (enableConfirmationPrompt && !src.includes('tos')) {
+                                        setShowCloseConfirmMessage(true)
+                                    } else {
+                                        onClose('manual')
+                                    }
+                                }}
+                            >
+                                CLOSE
+                            </button>
+                        )}
+                    </div>
+                    <div className="h-full w-full flex-grow overflow-scroll">
+                        <iframe
+                            key={src}
+                            src={src}
+                            allow="camera;"
+                            style={{ width: '100%', height: '80%', border: 'none' }}
+                            className="rounded-md"
+                            sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation"
+                        />
+                        <div className="flex h-1/5 w-full flex-col items-center justify-center gap-4 px-5">
+                            <Button
+                                variant={'purple'}
+                                className={`max-w-md`}
+                                onClick={() => {
+                                    setModalVariant('stop-verification')
+                                    setIsHelpModalOpen(true)
+                                }}
+                                shadowType="primary"
+                                shadowSize="4"
+                            >
+                                Stop verification process
+                            </Button>
+
+                            <button
+                                onClick={() => {
+                                    setModalVariant('trouble')
+                                    setIsHelpModalOpen(true)
+                                }}
+                                className="flex items-center gap-1"
+                            >
+                                <Icon name="peanut-support" size={16} />
+                                <p className="text-xs font-medium underline">Having trouble?</p>
+                            </button>
                         </div>
                     </div>
-                )}
-            </Modal>
-            {isHelpModalOpen && (
-                <ActionModal
-                    visible={isHelpModalOpen}
-                    onClose={() => setIsHelpModalOpen(false)}
-                    title={modalDetails.title}
-                    description={modalDetails.description}
-                    icon={modalDetails.icon}
-                    iconContainerClassName={modalDetails.iconContainerClassName}
-                    modalPanelClassName="max-w-full pointer-events-auto"
-                    ctaClassName="grid grid-cols-1 gap-3"
-                    modalClassName="!z-[10001] pointer-events-auto"
-                    preventClose={true}
-                    ctas={modalDetails.ctas}
-                />
+                </div>
             )}
-        </>
+            {/* {isHelpModalOpen && ( */}
+            <ActionModal
+                visible={isHelpModalOpen}
+                onClose={() => setIsHelpModalOpen(false)}
+                title={modalDetails.title}
+                description={modalDetails.description}
+                icon={modalDetails.icon}
+                iconContainerClassName={modalDetails.iconContainerClassName}
+                modalPanelClassName="max-w-full pointer-events-auto"
+                ctaClassName="grid grid-cols-1 gap-3"
+                modalClassName="!z-[10001] pointer-events-auto"
+                preventClose={true}
+                ctas={modalDetails.ctas}
+            />
+            {/* )} */}
+        </Modal>
     )
 }
 
