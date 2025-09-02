a23ee651fe75ad711bfc459e194d959e3863f501
Zishan Mohd <zishan2539@gmail.com>
2025-09-02 23:07:44 +0530

    fix: route typo and add kyc error and loading states

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -59,12 +59,12 @@ export const Profile = () => {
                         <ProfileMenuItem
                             icon="shield"
                             label="Identity Verification"
-                            href="/profile/indentity-verification"
+                            href="/profile/identity-verification"
                             onClick={() => {
                                 if (isKycApproved) {
                                     setIsKycApprovedModalOpen(true)
                                 } else {
-                                    router.push('/profile/indentity-verification')
+                                    router.push('/profile/identity-verification')
                                 }
                             }}
                             position="middle"
