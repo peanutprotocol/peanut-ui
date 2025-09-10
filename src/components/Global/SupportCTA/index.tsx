e5f8040a36f2d697261efed28ee333c889505b05
Zishan Mohd <zishan2539@gmail.com>
2025-09-10 16:55:10 +0530

    use next Link instead of a button for redirect

--- src/components/Global/SupportCTA/index.tsx
+++ src/components/Global/SupportCTA/index.tsx
@@ -1,25 +1,21 @@
 'use client'
 
 import { useAuth } from '@/context/authContext'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 const SupportCTA = () => {
-    const router = useRouter()
-    const { user } = useAuth()
+    const { user, isFetchingUser } = useAuth()
 
     // Show only to guest users
-    if (user) {
+    if (user || isFetchingUser) {
         return null
     }
 
     return (
         <div className="flex flex-col items-center justify-center border-t border-grey-1">
-            <p
-                onClick={() => router.push('/support')}
-                className="mt-2 cursor-pointer text-sm text-grey-1 underline underline-offset-2"
-            >
+            <Link href="/support" className="mt-2 cursor-pointer text-sm text-grey-1 underline underline-offset-2">
                 Need help with this transaction?
-            </p>
+            </Link>
         </div>
     )
 }
