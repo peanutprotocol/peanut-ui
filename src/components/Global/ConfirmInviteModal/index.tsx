207142ae59e8d05d12f285ea82d3129d00e16fee
Zishan Mohd <zishan2539@gmail.com>
2025-10-06 15:21:45 +0530

    add peanutman logo on top of modal

--- src/components/Global/ConfirmInviteModal/index.tsx
+++ src/components/Global/ConfirmInviteModal/index.tsx
@@ -1,7 +1,10 @@
+'use client'
 import React, { FC } from 'react'
-import ActionModal from '../ActionModal'
 import Image from 'next/image'
 import { PEANUT_LOGO_BLACK, PEANUTMAN_LOGO } from '@/assets'
+import Modal from '../Modal'
+import { Button } from '@/components/0_Bruddle'
+import chillPeanutAnim from '@/animations/GIF_ALPHA_BACKGORUND/512X512_ALPHA_GIF_konradurban_01.gif'
 
 interface ConfirmInviteModalProps {
     isOpen: boolean
@@ -19,38 +22,61 @@ const ConfirmInviteModal: FC<ConfirmInviteModalProps> = ({
     handleContinueWithPeanut,
 }) => {
     return (
-        <ActionModal
-            visible={isOpen}
-            title="Don’t lose your invite!"
-            titleClassName="font-extrabold text-lg"
-            description={`This link unlocks Peanut. Using ${method} will skip your invite.`}
-            onClose={onClose}
-            ctaClassName="flex-col sm:flex-col gap-0 md:gap-2"
-            contentContainerClassName="py-4 px-6"
-            ctas={[
-                {
-                    text: '',
-                    onClick: handleContinueWithPeanut,
-                    shadowSize: '4',
-                    className: 'sm:py-3',
-                    children: (
-                        <>
+        <div className="relative">
+            <Modal
+                hideOverlay
+                visible={isOpen}
+                onClose={onClose}
+                className="items-center rounded-none md:mx-auto md:max-w-md"
+                classWrap="sm:m-auto sm:self-center self-center m-4 bg-background rounded-none border-0"
+            >
+                {/* Main content container */}
+                <div className="relative z-10 w-full rounded-md bg-white px-6 py-6">
+                    <div className="space-y-4">
+                        <div className="space-y-3 text-center">
+                            <div className="w-full space-y-2">
+                                <h3 className={'text-xl font-extrabold text-black dark:text-white'}>
+                                    Don’t lose your invite!
+                                </h3>
+
+                                <div className="text-base text-grey-1 dark:text-white">
+                                    <p>
+                                        {' '}
+                                        This link lets you skip the Peanut waitlist. Using {method} will skip your
+                                        invite.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Button className="w-full" shadowSize="4" variant="purple" onClick={handleContinueWithPeanut}>
                             <div>Join</div>
                             <div className="flex items-center gap-1">
                                 <Image src={PEANUTMAN_LOGO} alt="Peanut Logo" className="size-5" />
                                 <Image src={PEANUT_LOGO_BLACK} alt="Peanut Logo" />
                             </div>
-                        </>
-                    ),
-                },
-                {
-                    text: `Continue with ${method}`,
-                    onClick: handleLoseInvite,
-                    variant: 'transparent',
-                    className: 'underline text-sm !font-normal w-full !transform-none !pt-2',
-                },
-            ]}
-        />
+                        </Button>
+                        <Button
+                            className="h-6 !transform-none !pt-2 text-sm !font-normal underline"
+                            variant="transparent"
+                            onClick={handleLoseInvite}
+                        >
+                            Continue with {method}
+                        </Button>
+                    </div>
+                </div>
+
+                {/* Peanutman animation */}
+                <div
+                    className="absolute left-0 top-7 flex w-full justify-center"
+                    style={{ transform: 'translateY(-80%)' }}
+                >
+                    <div className="relative h-42 w-[90%] md:h-52">
+                        <Image src={chillPeanutAnim.src} alt="Peanut Man" layout="fill" objectFit="contain" />
+                    </div>
+                </div>
+            </Modal>
+        </div>
     )
 }
 
