dbc0a61ed7a1826d7f156565b73fec58659e48f3
Juan José Ramírez <artjjrn@gmail.com>
2025-10-10 14:55:57 -0300

    refactor: dont shorten saved account titles

--- src/components/Common/SavedAccountsView.tsx
+++ src/components/Common/SavedAccountsView.tsx
@@ -98,8 +98,7 @@ export function SavedAccountsMapping({
                 else if (isFirst) position = 'first'
                 else if (isLast) position = 'last'
 
-                let title = account.type === AccountType.IBAN ? formatIban(account.identifier) : account.identifier
-                title = title.length > 20 ? shortenStringLong(account.identifier, 6) : title
+                const title = account.type === AccountType.IBAN ? formatIban(account.identifier) : account.identifier
 
                 return (
                     <SearchResultCard
