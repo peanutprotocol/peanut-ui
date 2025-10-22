59ff5887294ecad13e31b1f3ab5071074c0ded89
Zishan Mohd <zishan2539@gmail.com>
2025-10-22 18:55:18 +0530

    refactor: replace cloud animation logic with CloudImages component

--- src/components/LandingPage/Manteca.tsx
+++ src/components/LandingPage/Manteca.tsx
@@ -1,41 +1,12 @@
 import { motion } from 'framer-motion'
-import borderCloud from '@/assets/illustrations/border-cloud.svg'
 import { useEffect, useState } from 'react'
 import mantecaIphone from '@/assets/iphone-ss/manteca_ss.png'
 import Image from 'next/image'
 import { MEPA_ARGENTINA_LOGO, PIX_BRZ_LOGO, Star } from '@/assets'
+import { CloudImages } from './imageAssets'
 
 const Manteca = () => {
     const [screenWidth, setScreenWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 1200)
-
-    const createCloudAnimation = (
-        width: number,
-        speed: number,
-        delay: number = 0,
-        direction: 'left-to-right' | 'right-to-left' = 'left-to-right'
-    ) => {
-        const vpWidth = screenWidth || 1080
-        const totalDistance = vpWidth + width
-
-        return {
-            initial: { x: direction === 'left-to-right' ? -width : vpWidth },
-            animate: { x: direction === 'left-to-right' ? vpWidth : -width },
-            transition: {
-                ease: 'linear',
-                duration: totalDistance / speed,
-                repeat: Infinity,
-                delay: delay,
-            },
-        }
-    }
-
-    const cloudConfigs = [
-        { top: '10%', width: 180, speed: 30, delay: 0, direction: 'left-to-right' as const },
-        { top: '45%', width: 220, speed: 40, delay: 0, direction: 'left-to-right' as const },
-        { top: '80%', width: 210, speed: 38, delay: 0, direction: 'left-to-right' as const },
-        { top: '25%', width: 200, speed: 35, delay: 0, direction: 'right-to-left' as const },
-        { top: '65%', width: 190, speed: 32, delay: 0, direction: 'right-to-left' as const },
-    ]
 
     const starConfigs = [
         { className: 'absolute left-12 top-10', delay: 0.2 },
@@ -62,21 +33,7 @@ const Manteca = () => {
             className="relative overflow-hidden py-20 text-n-1 md:h-[850px] lg:h-[750px]"
             style={{ backgroundColor: '#F9F4F0' }}
         >
-            {!isMobile && (
-                <div className="absolute left-0 top-0 h-full w-full overflow-hidden">
-                    {/* Animated clouds */}
-                    {cloudConfigs.map((config, index) => (
-                        <motion.img
-                            key={index}
-                            src={borderCloud.src}
-                            alt="Floating Border Cloud"
-                            className="absolute left-0"
-                            style={{ top: config.top, width: config.width }}
-                            {...createCloudAnimation(config.width, config.speed, config.delay, config.direction)}
-                        />
-                    ))}
-                </div>
-            )}
+            {!isMobile && <CloudImages screenWidth={screenWidth} />}
 
             {!isMobile && (
                 <>
@@ -129,7 +86,7 @@ const Manteca = () => {
                 <div className="absolute -bottom-24 left-1/2 mx-auto flex -translate-x-1/2 items-center justify-center gap-20 lg:gap-36">
                     <Image src={MEPA_ARGENTINA_LOGO} alt="Mepa Argentina" width={170} height={170} />
                     <Image src={mantecaIphone} alt="Mercado pago payment" width={250} height={250} />
-                    <Image src={PIX_BRZ_LOGO} alt="Pix Brz" width={170} height={170} />
+                    <Image src={PIX_BRZ_LOGO} alt="Pix Brazil" width={170} height={170} />
                 </div>
             )}
         </section>
