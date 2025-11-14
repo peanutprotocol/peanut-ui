c9fdf3651073679aa559df19e96ff6e1e0e08081
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-11-14 15:36:57 -0300

    chore: update peanut icon to beta

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+// note: @dev incase you wanna change the icons,
+// better approach is to rename the image files so that browsers dont serve the cached old ones
 export default function manifest(): MetadataRoute.Manifest {
     let name = 'Peanut'
     switch (process.env.NODE_ENV) {
@@ -20,36 +22,36 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/icons/icon-192x192.png',
+                src: '/icons/icon-192x192-beta.png',
                 sizes: '192x192',
                 type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icons/icon-512x512.png',
+                src: '/icons/icon-512x512-beta.png',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icons/icon-192x192.png',
+                src: '/icons/icon-192x192-beta.png',
                 sizes: '192x192',
                 type: 'image/png',
                 purpose: 'any',
             },
             {
-                src: '/icons/icon-512x512.png',
+                src: '/icons/icon-512x512-beta.png',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'any',
             },
             {
-                src: '/icons/apple-touch-icon.png',
+                src: '/icons/apple-touch-icon-beta.png',
                 sizes: '180x180',
                 type: 'image/png',
             },
             {
-                src: '/icons/apple-touch-icon-152x152.png',
+                src: '/icons/apple-touch-icon-152x152-beta.png',
                 sizes: '152x152',
                 type: 'image/png',
             },
