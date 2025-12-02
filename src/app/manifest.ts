26f5fe2fd4ee5a56ea138d13633ee6b1a05ad9ce
Zishan Mohd <zishan2539@gmail.com>
2025-12-02 12:52:56 -0300

    fix: Update manifest to fix blach icon bg on android

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -27,18 +27,6 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/icons/icon-192x192-beta.png',
                 sizes: '192x192',
                 type: 'image/png',
-                purpose: 'maskable',
-            },
-            {
-                src: '/icons/icon-512x512-beta.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable',
-            },
-            {
-                src: '/icons/icon-192x192-beta.png',
-                sizes: '192x192',
-                type: 'image/png',
                 purpose: 'any',
             },
             {
