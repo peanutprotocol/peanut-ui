4087b42268532abe3fc649c0c941ed4049f74f40
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2025-12-10 13:46:44 +0530

    chore: revert comments

--- next.config.js
+++ next.config.js
@@ -200,15 +200,41 @@ if (process.env.NODE_ENV !== 'development' && !Boolean(process.env.LOCAL_BUILD))
 
     nextConfig = withSentryConfig(nextConfig, {
         telemetry,
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options
+
         org: 'peanut-c34d84c05',
         project: 'peanut-ui',
+
+        // Only print logs for uploading source maps in CI
+
+        // For all available options, see:
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+        // Upload a larger set of source maps for prettier stack traces (increases build time)
         widenClientFileUpload: true,
+
+        // Automatically annotate React components to show their full name in breadcrumbs and session replay
         reactComponentAnnotation: {
             enabled: true,
         },
+
+        // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+        // This can increase your server load as well as your hosting bill.
+        // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+        // side errors will fail.
         tunnelRoute: '/monitoring',
+
+        // Hides source maps from generated client bundles
         hideSourceMaps: true,
+
+        // Automatically tree-shake Sentry logger statements to reduce bundle size
         disableLogger: true,
+
+        // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+        // See the following for more information:
+        // https://docs.sentry.io/product/crons/
+        // https://vercel.com/docs/cron-jobs
         automaticVercelMonitors: true,
         sourcemaps: {
             deleteSourcemapsAfterUpload: true,
@@ -222,6 +248,7 @@ if (process.env.NODE_ENV !== 'development') {
         const withSerwist = (await import('@serwist/next')).default({
             swSrc: './src/app/sw.ts',
             swDest: 'public/sw.js',
+            // explicitly include offline screen assets in precache
             additionalPrecacheEntries: ['/icons/peanut-icon.svg'],
         })
         return withSerwist(nextConfig)
