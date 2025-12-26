43cd247ee1c050784f205f57adb6dba6a54fc34d
Zishan Mohd <zishan2539@gmail.com>
2025-12-26 13:28:43 +0530

    fix: refactor isMantecaCountry usage in AddWithdrawRouterView

--- src/components/AddWithdraw/AddWithdrawRouterView.tsx
+++ src/components/AddWithdraw/AddWithdrawRouterView.tsx
@@ -9,7 +9,7 @@ import { useUserStore } from '@/redux/hooks'
 import { AccountType, type Account } from '@/interfaces/interfaces'
 import { useWithdrawFlow } from '@/context/WithdrawFlowContext'
 import { useOnrampFlow } from '@/context/OnrampFlowContext'
-import { isMantecaCountry, MANTECA_COUNTRIES_CONFIG } from '@/constants/manteca.consts'
+import { isMantecaCountry } from '@/constants/manteca.consts'
 import Card from '@/components/Global/Card'
 import AvatarWithBadge from '@/components/Profile/AvatarWithBadge'
 import { CountryList } from '../Common/CountryList'
@@ -277,10 +277,8 @@ export const AddWithdrawRouterView: FC<AddWithdrawRouterViewProps> = ({
                 onCountryClick={(country) => {
                     // from send flow (bank): set method in context and stay on /withdraw?method=bank
                     if (flow === 'withdraw' && isBankFromSend) {
-                        const isMantecaCountry = country.id in MANTECA_COUNTRIES_CONFIG
-
-                        if (isMantecaCountry) {
-                            const route = `withdraw/manteca?method=bank-transfer&country=${country.path}`
+                        if (isMantecaCountry(country.path)) {
+                            const route = `/withdraw/manteca?method=bank-transfer&country=${country.path}`
                             startTransition(() => {
                                 router.push(route)
                             })
