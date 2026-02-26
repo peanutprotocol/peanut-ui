5a251a73dbe6256539a22c6888fa8636af174a32
Hugo Montenegro <h@hugo0.com>
2026-02-26 01:15:44 +0000

    fix: restore lockfile to zerodev 5.5.7, revert CashCard threshold (BE handles it)

--- src/components/Points/CashCard.tsx
+++ src/components/Points/CashCard.tsx
@@ -16,7 +16,7 @@ export const CashCard = ({ hasCashbackLeft, lifetimeEarned }: CashCardProps) => 
                     Lifetime cashback claimed: ${lifetimeEarned.toFixed(2)}
                 </h2>
                 <Tooltip
-                    content="The more points you have and higher Tier you are, the more cashback you get. The best way to get more cashback is to invite frinds!"
+                    content="The more points you have and higher Tier you are, the more cashback you get. The best way to get more cashback is to invite friends!"
                     position="bottom"
                 >
                     <Icon name="info" className="size-4 flex-shrink-0 text-grey-1" />
@@ -24,7 +24,7 @@ export const CashCard = ({ hasCashbackLeft, lifetimeEarned }: CashCardProps) => 
             </div>
 
             {hasCashbackLeft ? (
-                <p className="text-sm text-grey-1">You have more cashback left! Make a payment to claim it.</p>
+                <p className="text-sm text-grey-1">You have unclaimed cashback left! Make a payment to claim it.</p>
             ) : (
                 <p className="text-sm text-grey-1">Invite friends to unlock more cashback.</p>
             )}
