c680a887bcedb10f7ae3acbc4f9385a935cd448b
Hugo Montenegro <h@hugo0.com>
2026-06-29 02:34:11 -0700

    fix(card): drop the sparkle decoration from the launch banner

--- src/components/Home/CardLaunchCTA/CardLaunchCTABanner.tsx
+++ src/components/Home/CardLaunchCTA/CardLaunchCTABanner.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import Image from 'next/image'
 import { Icon } from '@/components/Global/Icons/Icon'
 import { Button } from '@/components/0_Bruddle/Button'
-import { Sparkle } from '@/assets/illustrations'
 import { useHaptic } from 'use-haptic'
 
 interface CardLaunchCTABannerProps {
@@ -51,15 +49,6 @@ export default function CardLaunchCTABanner({ onTryDoor, onDismiss }: CardLaunch
             >
                 <Icon name="cancel" size={16} />
             </button>
-
-            <Image
-                src={Sparkle}
-                alt=""
-                aria-hidden
-                width={56}
-                height={56}
-                className="pointer-events-none absolute -right-3 -top-3 w-12 select-none md:w-14"
-            />
 
             <div className="relative z-[1] flex flex-col gap-3 pr-6">
                 <span className="font-roboto-flex-extrabold text-[11px] font-extraBlack uppercase tracking-[0.22em] text-n-1 opacity-60">
