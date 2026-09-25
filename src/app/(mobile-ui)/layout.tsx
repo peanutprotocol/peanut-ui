1b40a8b8abfd433fb3b6b088ec31eb00dc01c0db
innolope-dev <bianco.elefante@gmail.com>
2026-09-25 12:43:15 +0100

    fix(help): provide article drawers across app routes

--- src/app/(mobile-ui)/layout.tsx
+++ src/app/(mobile-ui)/layout.tsx
@@ -1,13 +1,6 @@
-import { AppHelpProvider } from '@/components/Global/AppHelpDrawer'
-import { loadAppHelpDocuments } from '@/components/Global/appHelpDocuments.server'
 import MobileLayoutClient from './MobileLayoutClient'
 import '../../styles/globals.css'
 
-export default async function MobileLayout({ children }: { children: React.ReactNode }) {
-    const documents = await loadAppHelpDocuments()
-    return (
-        <AppHelpProvider documents={documents}>
-            <MobileLayoutClient>{children}</MobileLayoutClient>
-        </AppHelpProvider>
-    )
+export default function MobileLayout({ children }: { children: React.ReactNode }) {
+    return <MobileLayoutClient>{children}</MobileLayoutClient>
 }
